a22e729f50979da2d121225676bd83683a3d5b9d
atrtdeee <trotelalexandre@proton.me>
2023-10-10 20:34:34 +0200

    fixing types

--- components/stateless/modules.tsx
+++ components/stateless/modules.tsx
@@ -11,7 +11,7 @@ interface Module {
   status: "ready" | "finished" | "ongoing";
   statusText: string;
   description: string;
-  environment: "Ready" | "Finished" | "Ongoing";
+  environment: "Coming Soon" | "Ready" | "Finished" | "Ongoing";
   submodules: Submodule[];
 }
 
@@ -22,7 +22,7 @@ interface Submodule {
   module: string;
   status: "ready" | "finished" | "ongoing";
   statusText: string;
-  environment: "Ready" | "Finished" | "Ongoing";
+  environment: "Coming Soon" | "Ready" | "Finished" | "Ongoing";
   description: string;
 }
 
@@ -55,7 +55,7 @@ const modules: Module[] = [
     statusText: "0%",
     description:
       "Learn how to set up and manage your digital wallet for secure and convenient financial transactions",
-    environment: "Ready",
+    environment: "Coming Soon",
     submodules: [
       {
         id: 11,
@@ -65,7 +65,7 @@ const modules: Module[] = [
         statusText: "0%",
         description:
           "Get an overview of digital wallets and their importance in the world of cryptocurrency",
-        environment: "Ready",
+        environment: "Coming Soon",
       },
       {
         id: 12,
@@ -75,7 +75,7 @@ const modules: Module[] = [
         statusText: "0%",
         description:
           "Learn best practices for securing your digital wallet and keeping your cryptocurrencies safe",
-        environment: "Ready",
+        environment: "Coming Soon",
       },
     ],
   },
@@ -88,7 +88,7 @@ const modules: Module[] = [
     statusText: "0%",
     description:
       "Explore the world of Bitcoin and gain a comprehensive understanding of its technology, uses, and implications",
-    environment: "Ready",
+    environment: "Coming Soon",
     submodules: [
       {
         id: 21,
@@ -98,7 +98,7 @@ const modules: Module[] = [
         statusText: "0%",
         description:
           "Learn the basics of Bitcoin, its history, and its underlying technology, blockchain",
-        environment: "Ready",
+        environment: "Coming Soon",
       },
       {
         id: 22,
@@ -108,7 +108,7 @@ const modules: Module[] = [
         statusText: "0%",
         description:
           "Dive into the process of Bitcoin mining and how it supports the network",
-        environment: "Ready",
+        environment: "Coming Soon",
       },
     ],
   },
@@ -121,7 +121,7 @@ const modules: Module[] = [
     statusText: "0%",
     description:
       "Delve into the realm of Ethereum to uncover its technology, applications, and potential impact on the future of decentralized computing and finances",
-    environment: "Ready",
+    environment: "Coming Soon",
     submodules: [
       {
         id: 31,
@@ -131,7 +131,7 @@ const modules: Module[] = [
         statusText: "0%",
         description:
           "Get introduced to Ethereum, its founder, and its goals in the world of blockchain technology",
-        environment: "Ready",
+        environment: "Coming Soon",
       },
       {
         id: 32,
@@ -141,7 +141,7 @@ const modules: Module[] = [
         statusText: "0%",
         description:
           "Explore the concept of smart contracts and their role in decentralized applications (DApps)",
-        environment: "Ready",
+        environment: "Coming Soon",
       },
     ],
   },
@@ -155,7 +155,9 @@ function classNames(...classes: string[]) {
 // Define the Modules component
 export default function Modules() {
   const [expandedModules, setExpandedModules] = useState<number[]>([]);
-  const [selectedModule, setSelectedModule] = useState<Module | null>(null);
+  const [selectedModule, setSelectedModule] = useState<
+    Module | Submodule | null
+  >(null);
 
   // Handle click event to open or close a module
   const handleModuleClick = (module: Module) => {
@@ -163,6 +165,15 @@ export default function Modules() {
       setSelectedModule(null);
     } else {
       setSelectedModule(module);
+    }
+  };
+
+  // Handle click event to open or close a submodule
+  const handleSubModuleClick = (submodule: Submodule) => {
+    if (selectedModule === submodule) {
+      setSelectedModule(null);
+    } else {
+      setSelectedModule(submodule);
     }
   };
 
@@ -246,67 +257,71 @@ export default function Modules() {
               selectedModule ? "block" : "hidden"
             }`}
           >
-            {selectedModule.submodules.map((submodule) => (
-              <li
-                key={submodule.id}
-                className="relative flex items-center space-x-4 py-4 cursor-pointer"
-                onClick={() => handleModuleClick(submodule)}
-              >
-                {/* Submodule content */}
-                <div className="min-w-0 flex-auto">
-                  <div className="flex items-center gap-x-3">
-                    <div
-                      className={classNames(
-                        statuses[submodule.status],
-                        "flex-none rounded-full p-1"
-                      )}
-                    >
-                      <div className="h-2 w-2 rounded-full bg-current" />
+            {(selectedModule as Module).submodules.map(
+              (submodule: Submodule) => (
+                <li
+                  key={submodule.id}
+                  className="relative flex items-center space-x-4 py-4 cursor-pointer"
+                  onClick={() => handleSubModuleClick(submodule)}
+                >
+                  {/* Submodule content */}
+                  <div className="min-w-0 flex-auto">
+                    <div className="flex items-center gap-x-3">
+                      <div
+                        className={classNames(
+                          statuses[submodule.status],
+                          "flex-none rounded-full p-1"
+                        )}
+                      >
+                        <div className="h-2 w-2 rounded-full bg-current" />
+                      </div>
+                      <h2 className="min-w-0 text-sm font-semibold leading-6 text-gray-900 dark:text-gray-100">
+                        <a href={submodule.href} className="flex gap-x-2">
+                          <span className="whitespace-nowrap">
+                            {submodule.module}
+                          </span>
+                          <span className="absolute inset-0" />
+                        </a>
+                      </h2>
                     </div>
-                    <h2 className="min-w-0 text-sm font-semibold leading-6 text-gray-900 dark:text-gray-100">
-                      <a href={submodule.href} className="flex gap-x-2">
-                        <span className="whitespace-nowrap">
-                          {submodule.module}
-                        </span>
-                        <span className="absolute inset-0" />
-                      </a>
-                    </h2>
+                    <div className="mt-3 flex items-center gap-x-2.5 text-xs leading-5 text-gray-600 dark:text-gray-400">
+                      <p className="truncate">{submodule.description}</p>
+                      <svg
+                        viewBox="0 0 2 2"
+                        className="h-0.5 w-0.5 flex-none fill-gray-500 dark:fill-gray-300"
+                      >
+                        <circle cx={1} cy={1} r={1} />
+                      </svg>
+                      <p className="whitespace-nowrap">
+                        {submodule.statusText}
+                      </p>
+                    </div>
                   </div>
-                  <div className="mt-3 flex items-center gap-x-2.5 text-xs leading-5 text-gray-600 dark:text-gray-400">
-                    <p className="truncate">{submodule.description}</p>
-                    <svg
-                      viewBox="0 0 2 2"
-                      className="h-0.5 w-0.5 flex-none fill-gray-500 dark:fill-gray-300"
-                    >
-                      <circle cx={1} cy={1} r={1} />
-                    </svg>
-                    <p className="whitespace-nowrap">{submodule.statusText}</p>
+                  <div
+                    className={classNames(
+                      environments[submodule.environment],
+                      "hidden lg:flex rounded-full flex-none py-1 px-2 text-xs font-medium ring-1 ring-inset"
+                    )}
+                  >
+                    {submodule.environment}
                   </div>
-                </div>
-                <div
-                  className={classNames(
-                    environments[submodule.environment],
-                    "hidden lg:flex rounded-full flex-none py-1 px-2 text-xs font-medium ring-1 ring-inset"
-                  )}
-                >
-                  {submodule.environment}
-                </div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="h-5 w-5 flex-none text-gray-600 dark:text-gray-400"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-                  />
-                </svg>
-              </li>
-            ))}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="h-5 w-5 flex-none text-gray-600 dark:text-gray-400"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                    />
+                  </svg>
+                </li>
+              )
+            )}
           </ul>
         </Slide>
       );
@@ -318,7 +333,7 @@ export default function Modules() {
   return (
     <div className="lg:mx-10 mx-4 my-8">
       <Fade>
-        <div className="lg:mx-10 mx-4 my-8 shadow border-2 border-gray-900 dark:border-transparent rounded-xl bg-gray-50 dark:bg-gray-800 px-10 py-5 overflow-hidden">
+        <div className="shadow border-2 border-gray-900 dark:border-transparent rounded-xl bg-gray-50 dark:bg-gray-800 px-10 py-5 overflow-hidden">
           <h2 className="text-xl text-gray-900 dark:text-gray-100 font-semibold">
             {selectedModule ? selectedModule.module : "Courses"}
           </h2>
