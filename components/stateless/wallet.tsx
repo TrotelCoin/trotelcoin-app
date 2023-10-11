d2d5741c149044b2eaf0726cd3f19270aae2539e
atrtdeee <trotelalexandre@proton.me>
2023-10-11 14:14:37 +0200

    moralis auth

--- components/stateless/wallet.tsx
+++ components/stateless/wallet.tsx
@@ -1,58 +1,55 @@
 import React, { useEffect, useState } from "react";
-import { useWeb3Modal } from "@web3modal/wagmi/react";
-import { useSignMessage, useAccount } from "wagmi";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { useAccount, useConnect, useSignMessage, useDisconnect } from "wagmi";
+import { useAuthRequestChallengeEvm } from "@moralisweb3/next";
+import { useRouter } from "next/router";
+import { SignInResponse, signIn } from "next-auth/react";
 
 export default function Wallet() {
-  const { open } = useWeb3Modal();
+  const { connectAsync } = useConnect();
+  const { disconnectAsync } = useDisconnect();
   const { isConnected } = useAccount();
-  const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage({
-    message: "Log in to the TrotelCoin platform.",
-  });
+  const { signMessageAsync } = useSignMessage();
+  const { requestChallengeAsync } = useAuthRequestChallengeEvm();
+  const { push } = useRouter();
 
-  const [hasSigned, setHasSigned] = useState(false); // State to track whether the user has signed
-
-  // Check local storage to see if the user has signed
-  useEffect(() => {
-    const hasSignedLocally = localStorage.getItem("hasSigned");
-    if (hasSignedLocally === "true") {
-      setHasSigned(true);
+  const handleAuth = async () => {
+    if (isConnected) {
+      await disconnectAsync();
     }
-  }, []);
 
-  // Update the button text based on the user's wallet connection status
-  useEffect(() => {
-    const button = document.querySelector(".wallet-button");
-    if (button) {
-      if (isConnected) {
-        button.textContent = isSuccess ? "My wallet" : "Log in";
-      } else {
-        button.textContent = "Connect Wallet";
-      }
-    }
-  }, [isConnected, isSuccess]);
+    const { account, chain } = await connectAsync({
+      connector: new MetaMaskConnector(),
+    });
 
-  // Handle the sign message action
-  const handleSignMessage = async () => {
-    if (isConnected && !isSuccess) {
-      if (!hasSigned) {
-        await signMessage();
-        // Set the flag to indicate the user has signed
-        localStorage.setItem("hasSigned", "true");
-        setHasSigned(true);
-      }
-    } else {
-      open();
-    }
+    const { message } = (await requestChallengeAsync({
+      address: account,
+      chainId: chain.id,
+    })) as { id: string; profileId: string; message: string };
+
+    const signature = await signMessageAsync({ message });
+
+    // redirect user after success authentication to '/user' page
+    const { url } = (await signIn("moralis-auth", {
+      message,
+      signature,
+      redirect: false,
+      callbackUrl: "/user",
+    })) as SignInResponse;
+    /**
+     * instead of using signIn(..., redirect: "/user")
+     * we get the url from callback and push it to the router to avoid page refreshing
+     */
+    push(url as string);
   };
 
   return (
     <div>
       <button
         className={`bg-yellow-200 border-2 border-gray-900 dark:border-transparent hover:bg-yellow-100 dark:hover:bg-yellow-50 text-sm px-6 py-2 dark:bg-yellow-100 text-gray-900 dark:text-gray-900 dark:hover:text-gray-900 hover:text-gray-900 rounded-full font-semibold wallet-button`}
-        disabled={isLoading}
-        onClick={handleSignMessage}
+        onClick={handleAuth}
       >
-        {isConnected ? (isSuccess ? "My wallet" : "Log in") : "Connect Wallet"}
+        {isConnected ? "My wallet" : "Connect Wallet"}
       </button>
     </div>
   );
--- pages/api/moralis/[...moralis].ts
+++ pages/api/moralis/[...moralis].ts
@@ -3,7 +3,9 @@ import { MoralisNextApi } from "@moralisweb3/next";
 export default MoralisNextApi({
   apiKey: process.env.MORALIS_API_KEY as string,
   authentication: {
-    domain: "app.trotelcoin.com",
+    domain: process.env.NEXTAUTH_URL
+      ? new URL(process.env.NEXTAUTH_URL).host
+      : "",
     uri: process.env.NEXTAUTH_URL as string,
     timeout: 120,
   },
