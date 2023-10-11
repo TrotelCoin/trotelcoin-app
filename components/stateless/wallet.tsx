83b84879c4f3e358c26cab4537a5b4ff5f63ec4c
atrtdeee <trotelalexandre@proton.me>
2023-10-11 15:30:15 +0200

    adding disconnect

--- components/stateless/wallet.tsx
+++ components/stateless/wallet.tsx
@@ -7,10 +7,15 @@ import { SignInResponse, signIn } from "next-auth/react";
 
 export default function Wallet() {
   const { connectAsync } = useConnect();
+  const { disconnectAsync } = useDisconnect();
   const { isConnected } = useAccount();
   const { signMessageAsync } = useSignMessage();
   const { requestChallengeAsync } = useAuthRequestChallengeEvm();
   const { push } = useRouter();
+
+  const handleDisconnect = async () => {
+    disconnectAsync();
+  };
 
   const handleAuth = async () => {
     const { account, chain } = await connectAsync({
@@ -43,7 +48,7 @@ export default function Wallet() {
       <div>
         <button
           className={`bg-yellow-200 border-2 border-gray-900 dark:border-transparent hover:bg-yellow-100 dark:hover:bg-yellow-50 text-sm px-6 py-2 dark:bg-yellow-100 text-gray-900 dark:text-gray-900 dark:hover:text-gray-900 hover:text-gray-900 rounded-full font-semibold wallet-button`}
-          onClick={() => useDisconnect()}
+          onClick={handleDisconnect}
         >
           Disconnect
         </button>
