516320dad2f2eeef27c0f40fd7d749a0df175e54
atrtdeee <trotelalexandre@proton.me>
2023-10-16 11:41:26 +0200

    forgot 2 buttons

--- components/modals/success.tsx
+++ components/modals/success.tsx
@@ -66,7 +66,7 @@ const Success: React.FC<SuccessProps> = ({ title, show, message, onClose }) => {
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-full bg-yellow-200 dark:hover:bg-yellow-50 hover:bg-yellow-100 dark:bg-yellow-100 dark:hover-bg-yellow-50 border-2 border-gray-900/10 dark:border-gray-100/10 px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm"
+                    className="inline-flex w-full justify-center rounded-full bg-yellow-200 dark:hover:bg-yellow-50 hover:bg-yellow-100 dark:bg-yellow-100 dark:hover-bg-yellow-50 px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm"
                     onClick={onClose}
                   >
                     Close
