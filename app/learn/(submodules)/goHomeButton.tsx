70a985b86664c4c6a09f9c2ae115b3e5b5ff799c
trotelalexandre <trotelalexandre@proton.me>
2023-11-03 11:45:35 +0100

    fixing learn redirection

--- app/learn/(submodules)/goHomeButton.tsx
+++ app/learn/(submodules)/goHomeButton.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import React from "react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function GoHomeButton() {
-  const router = useRouter();
-
   return (
-    <button
-      className="absolute top-5 left-5 bg-blue-200 dark:hover:bg-blue-200/80 hover:bg-blue-200/80 dark:hover-bg-blue-50 text-sm px-6 py-2 dark:bg-blue-200 text-gray-900 dark:text-gray-900 dark:hover:text-gray-900 hover:text-gray-900 rounded-full font-semibold"
-      onClick={() => router.push("/learn")}
-    >
-      &larr;<span className="ml-4">Go back</span>
-    </button>
+    <Link href="/learn">
+      <button className="absolute top-5 left-5 bg-blue-200 dark:hover:bg-blue-200/80 hover:bg-blue-200/80 dark:hover-bg-blue-50 text-sm px-6 py-2 dark:bg-blue-200 text-gray-900 dark:text-gray-900 dark:hover:text-gray-900 hover:text-gray-900 rounded-full font-semibold">
+        &larr;<span className="ml-4">Go back</span>
+      </button>
+    </Link>
   );
 }
