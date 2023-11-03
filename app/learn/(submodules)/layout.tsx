4f98efbc84b73f845205243699b02ac423a2290c
trotelalexandre <trotelalexandre@proton.me>
2023-11-03 16:44:53 +0100

    fixing layout

--- app/learn/(submodules)/layout.tsx
+++ app/learn/(submodules)/layout.tsx
@@ -10,8 +10,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <body
         className={`bg-white dark:bg-black ${poppins.className} antialiased`}
       >
-        {children}
-        <GoHomeButton />
+        <div className="bg-white dark:bg-black px-6 py-32 lg:px-8">
+          <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700 dark:text-gray-300">
+            {children}
+            <GoHomeButton />
+          </div>
+        </div>
       </body>
     </html>
   );
