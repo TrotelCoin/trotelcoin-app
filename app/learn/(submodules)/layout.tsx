21a963312839d443adb6da099927db54c7ec1140
trotelalexandre <trotelalexandre@proton.me>
2023-11-03 12:00:04 +0100

    layout fix

--- app/learn/(submodules)/layout.tsx
+++ app/learn/(submodules)/layout.tsx
@@ -1,12 +1,15 @@
 // Import necessary modules and components
 import React from "react";
 import GoHomeButton from "@/app/learn/(submodules)/goHomeButton";
+import { poppins } from "@/app/ui/fonts";
 
 // Define the main Document component
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <body>
+      <body
+        className={`bg-white dark:bg-black ${poppins.className} antialiased`}
+      >
         <GoHomeButton />
         {children}
       </body>
