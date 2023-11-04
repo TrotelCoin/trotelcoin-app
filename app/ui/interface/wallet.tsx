68b223883781bb10ffb71edf011b059c910924a7
trotelalexandre <trotelalexandre@proton.me>
2023-11-04 19:18:42 +0100

    disconnect when component unmount

--- app/ui/interface/wallet.tsx
+++ app/ui/interface/wallet.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
 import { unstable_noStore as noStore } from "next/cache";
@@ -29,32 +29,34 @@ export default function Wallet() {
     }
   };
 
+  useEffect(() => {
+    return () => {
+      handleDisconnect();
+    };
+  }, [handleDisconnect]);
+
   // Return the component based on state
   if (isConnected) {
     return (
-      <>
-        <div>
-          <button
-            className={`bg-blue-200 dark:hover:bg-blue-200/80 hover:bg-blue-200/80 dark:hover-bg-blue-50 text-sm px-6 py-2 dark:bg-blue-200 text-gray-900 dark:text-gray-900 dark:hover:text-gray-900 hover:text-gray-900 rounded-full font-semibold wallet-button`}
-            onClick={handleDisconnect}
-          >
-            Sign out
-          </button>
-        </div>
-      </>
+      <div>
+        <button
+          className={`bg-blue-200 dark:hover:bg-blue-200/80 hover:bg-blue-200/80 dark:hover-bg-blue-50 text-sm px-6 py-2 dark:bg-blue-200 text-gray-900 dark:text-gray-900 dark:hover:text-gray-900 hover:text-gray-900 rounded-full font-semibold wallet-button`}
+          onClick={handleDisconnect}
+        >
+          Sign out
+        </button>
+      </div>
     );
   } else {
     return (
-      <>
-        <div>
-          <button
-            className={`bg-blue-200 dark:hover:bg-blue-200/80 hover:bg-blue-200/80 dark:hover-bg-blue-50 text-sm px-6 py-2 dark:bg-blue-200 text-gray-900 dark:text-gray-900 dark:hover:text-gray-900 hover:text-gray-900 rounded-full font-semibold wallet-button`}
-            onClick={handleAuth}
-          >
-            Log in
-          </button>
-        </div>
-      </>
+      <div>
+        <button
+          className={`bg-blue-200 dark:hover:bg-blue-200/80 hover:bg-blue-200/80 dark:hover-bg-blue-50 text-sm px-6 py-2 dark:bg-blue-200 text-gray-900 dark:text-gray-900 dark:hover:text-gray-900 hover:text-gray-900 rounded-full font-semibold wallet-button`}
+          onClick={handleAuth}
+        >
+          Log in
+        </button>
+      </div>
     );
   }
 }
