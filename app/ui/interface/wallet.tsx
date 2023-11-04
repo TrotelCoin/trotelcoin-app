56181ef765d282be4a4a4d8e7952d168b710289c
trotelalexandre <trotelalexandre@proton.me>
2023-11-04 18:54:37 +0100

    changing wallet logic

--- app/ui/interface/wallet.tsx
+++ app/ui/interface/wallet.tsx
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { useAccount, useConnect, useSignMessage, useDisconnect } from "wagmi";
-import Success from "@/app/ui/modals/success";
-import Fail from "@/app/ui/modals/fail";
 
 export default function Wallet() {
   const { connectAsync } = useConnect();
@@ -12,16 +10,10 @@ export default function Wallet() {
     message: "Log into the TrotelCoin platform.",
   });
 
-  const [connect, setConnect] = useState<boolean>(false);
-  const [disconnect, setDisconnect] = useState<boolean>(false);
-  const [error, setError] = useState<boolean>(false);
-
   const handleDisconnect = async () => {
     try {
       await disconnectAsync();
-      setDisconnect(true);
     } catch (error) {
-      setError(true);
       console.error("Disconnect error:", error);
     }
   };
@@ -33,52 +25,16 @@ export default function Wallet() {
       });
 
       signMessage();
-      setConnect(true);
       console.log("Account has been connected:", account);
     } catch (error) {
-      setError(true);
       console.error("Authentication error:", error);
     }
   };
-
-  const closeSuccess = () => {
-    setConnect(false);
-  };
-
-  const closeDisconnect = () => {
-    setDisconnect(false);
-  };
-
-  const closeError = () => {
-    setError(false);
-  };
-
-  // Use useEffect to trigger actions only when the component mounts initially
-  useEffect(() => {
-    // Check isConnected status on initial mount, set connect/disconnect state accordingly
-    if (isConnected) {
-      setConnect(true);
-    } else {
-      setDisconnect(true);
-    }
-  }, [isConnected]);
 
   // Return the component based on state
   if (isConnected) {
     return (
       <>
-        <Success
-          title="Connected"
-          show={connect}
-          message="You logged into TrotelCoin!"
-          onClose={closeSuccess}
-        />
-        <Fail
-          title="Connection error"
-          show={error}
-          message="Unknown error!"
-          onClose={closeError}
-        />
         <div>
           <button
             className={`bg-blue-200 dark:hover:bg-blue-200/80 hover:bg-blue-200/80 dark:hover-bg-blue-50 text-sm px-6 py-2 dark:bg-blue-200 text-gray-900 dark:text-gray-900 dark:hover:text-gray-900 hover:text-gray-900 rounded-full font-semibold wallet-button`}
@@ -92,12 +48,6 @@ export default function Wallet() {
   } else {
     return (
       <>
-        <Success
-          title="Disconnected"
-          show={disconnect}
-          message="You logged out of TrotelCoin!"
-          onClose={closeDisconnect}
-        />
         <div>
           <button
             className={`bg-blue-200 dark:hover:bg-blue-200/80 hover:bg-blue-200/80 dark:hover-bg-blue-50 text-sm px-6 py-2 dark:bg-blue-200 text-gray-900 dark:text-gray-900 dark:hover:text-gray-900 hover:text-gray-900 rounded-full font-semibold wallet-button`}
