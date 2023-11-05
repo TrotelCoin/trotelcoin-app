f710f48105b6d7f9495469f34fcbfc9cd4f08fed
trotelalexandre <trotelalexandre@proton.me>
2023-11-05 13:32:00 +0100

    fixing goHomeButton + remove html from sub layout

--- app/(lessons)/layout.tsx
+++ app/(lessons)/layout.tsx
@@ -5,15 +5,11 @@ import GoHomeButton from "@/app/ui/interface/lessons/goHomeButton";
 // Define the main Document component
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
-      <body>
-        <div className="bg-white dark:bg-black px-6 py-32 lg:px-8">
-          <div className="mx-auto max-w-3xl text-base leading-7 text-stone-700 dark:text-stone-300">
-            {children}
-            <GoHomeButton />
-          </div>
-        </div>
-      </body>
-    </html>
+    <div className="px-6 py-32 lg:px-8">
+      <div className="mx-auto max-w-3xl text-base leading-7 text-stone-700 dark:text-stone-300">
+        {children}
+        <GoHomeButton />
+      </div>
+    </div>
   );
 }
--- app/ui/interface/lessons/goHomeButton.tsx
+++ app/ui/interface/lessons/goHomeButton.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 export default function GoHomeButton() {
   return (
-    <Link href="/learn">
-      <button className="flex mt-10 bg-blue-200 dark:hover:bg-blue-200/80 hover:bg-blue-200/80 dark:hover-bg-blue-50 text-sm px-6 py-2 dark:bg-blue-200 text-gray-900 dark:text-gray-900 dark:hover:text-gray-900 hover:text-gray-900 rounded-full font-semibold">
+    <Link href="/">
+      <button className="flex mt-10 bg-gray-50 dark:bg-gray-900 hover:shadow focus:shadow-none focus:border-blue-600 dark:focus:border-blue-200 dark:hover-bg-blue-50 text-sm px-6 py-2 text-gray-900 dark:text-gray-100 hover:text-gray-900 rounded-lg font-semibold">
         &larr;<span className="ml-4">Go back</span>
       </button>
     </Link>
