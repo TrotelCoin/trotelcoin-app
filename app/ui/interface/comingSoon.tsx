f7e82d11a94ea651974de0e1e790e4eff968ce3d
trotelalexandre <trotelalexandre@proton.me>
2023-11-05 13:41:18 +0100

    reducing coming soon

--- app/ui/interface/comingSoon.tsx
+++ app/ui/interface/comingSoon.tsx
@@ -9,7 +9,7 @@ const ComingSoon = () => {
         {/* Card with a "Coming Soon" message */}
         <div className="bg-gray-50 border backdrop-blur-xl border-gray-900/10 dark:border-gray-100/10 text-center rounded-lg p-10 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
           {/* Text displaying "Coming Soon" */}
-          <span className="text-gray-900 dark:text-gray-100 animate-pulse text-4xl lg:text-8xl">
+          <span className="text-gray-900 dark:text-gray-100 animate-pulse text-xl">
             Coming soon
           </span>
         </div>
