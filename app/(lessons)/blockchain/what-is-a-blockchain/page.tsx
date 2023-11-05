7a6bff807954f605a13872b3e9530bf4935b43ce
trotelalexandre <trotelalexandre@proton.me>
2023-11-05 17:50:30 +0100

    removing hover

--- app/(lessons)/blockchain/what-is-a-blockchain/page.tsx
+++ app/(lessons)/blockchain/what-is-a-blockchain/page.tsx
@@ -21,7 +21,7 @@ const CoursePage = () => {
         <p className="mt-2 text-gray-900 dark:text-gray-100">
           What are you going to learn?
         </p>
-        <div className="bg-gray-50 my-10 border backdrop-blur-xl border-gray-900/10 dark:border-gray-100/10 rounded-lg px-10 py-2 dark:bg-gray-900 hover:border-gray-900/50 dark:hover:border-gray-100/50">
+        <div className="bg-gray-50 my-10 border backdrop-blur-xl border-gray-900/10 dark:border-gray-100/10 rounded-lg px-10 py-2 dark:bg-gray-900">
           <ul
             role="list"
             className="max-w-xl space-y-8 text-gray-700 dark:text-gray-300"
