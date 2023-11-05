4990287dac39543107d655d4d1b0c0cdec51bc1f
trotelalexandre <trotelalexandre@proton.me>
2023-11-05 13:42:53 +0100

    connect wallet wording

--- app/ui/interface/wallet.tsx
+++ app/ui/interface/wallet.tsx
@@ -20,7 +20,7 @@ export default function Wallet() {
           className={`bg-blue-600 dark:bg-blue-200 hover:bg-blue-600/80 dark:hover:bg-blue-200/80 dark:hover:bg-blue-50 px-6 py-2 text-sm text-gray-100 dark:text-gray-900 dark:hover:text-gray-900 hover:text-gray-100 rounded-full font-semibold wallet-button`}
           onClick={handleDisconnect}
         >
-          Sign out
+          Disconnect
         </button>
       </div>
     );
@@ -31,7 +31,7 @@ export default function Wallet() {
           className={`bg-blue-600 dark:bg-blue-200 hover:bg-blue-600/80 dark:hover:bg-blue-200/80 dark:hover:bg-blue-50 px-6 py-2 text-sm text-gray-100 dark:text-gray-900 dark:hover:text-gray-900 hover:text-gray-100 rounded-full font-semibold wallet-button`}
           onClick={handleAuth}
         >
-          Log in
+          Connect wallet
         </button>
       </div>
     );
