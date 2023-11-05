9531066a9362afb114cbd1b0c656ba4382fb4f76
trotelalexandre <trotelalexandre@proton.me>
2023-11-05 13:50:25 +0100

    coming soon

--- app/ui/interface/comingSoon.tsx
+++ app/ui/interface/comingSoon.tsx
@@ -4,15 +4,12 @@ import React from "react";
 const ComingSoon = () => {
   return (
     <>
-      {/* Container for the "Coming Soon" message */}
-      <div className="flex justify-center mx-10 lg:mx-32 my-20 overflow-hidden">
-        {/* Card with a "Coming Soon" message */}
-        <div className="bg-gray-50 border backdrop-blur-xl border-gray-900/10 dark:border-gray-100/10 hover:border-gray-900/50 dark:hover:border-gray-100/50 text-center rounded-lg p-10 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
-          {/* Text displaying "Coming Soon" */}
-          <span className="text-gray-900 dark:text-gray-100 animate-pulse text-xl">
-            Coming soon
-          </span>
-        </div>
+      {/* Card with a "Coming Soon" message */}
+      <div className="block mx-auto w-full my-20 overflow-hidden bg-gray-50 border backdrop-blur-xl border-gray-900/10 dark:border-gray-100/10 hover:border-gray-900/50 dark:hover:border-gray-100/50 text-center rounded-lg p-10 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+        {/* Text displaying "Coming Soon" */}
+        <span className="text-gray-900 dark:text-gray-100 animate-pulse text-xl">
+          Coming soon
+        </span>
       </div>
     </>
   );
