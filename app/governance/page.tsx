e0aa7aff6032fd02413dd583c938aa43c59f59db
trotelalexandre <trotelalexandre@proton.me>
2023-11-07 19:43:52 +0100

    fixing

--- app/governance/page.tsx
+++ app/governance/page.tsx
@@ -110,82 +110,80 @@ export default function Governance() {
           <span className="font-semibold">GovTrotelCoin.</span>
         </span>
       </h1>
-      <div className="flex justify-center">
-        <h2 className="font-semibold mt-10 text-gray-900 dark:text-gray-100">
-          Stake
-        </h2>
-        <div className="flex flex-col mt-4 items-center gap-6">
-          <div className="flex gap-4">
-            <input
-              className="px-4 py-2 rounded-lg"
-              placeholder="Amount"
-              value={inputValue}
-              onChange={handleInputChange}
-            ></input>
-            {!confirmStaking && (
-              <button
-                className="flex border border-gray-900/10 dark:border-gray-100/10 bg-gray-50 dark:bg-gray-900 hover:shadow hover:border-gray-900/50 dark:hover:border-gray-100/50 focus:shadow-none focus:border-blue-600 dark:focus:border-blue-200 dark:hover-bg-blue-50 text-sm px-6 py-2 text-gray-900 dark:text-gray-100 rounded-lg font-semibold"
-                onClick={handleStake}
-              >
-                Stake
-              </button>
-            )}
-            {confirmStaking && (
-              <button
-                className="flex border border-gray-900/10 dark:border-gray-100/10 bg-gray-50 dark:bg-gray-900 hover:shadow hover:border-gray-900/50 dark:hover:border-gray-100/50 focus:shadow-none focus:border-blue-600 dark:focus:border-blue-200 dark:hover-bg-blue-50 text-sm px-6 py-2 text-gray-900 dark:text-gray-100 rounded-lg font-semibold"
-                onClick={handleConfirm}
-              >
-                Confirm
-              </button>
-            )}
-          </div>
-          {warningMessage && (
-            <span className="animate__animated animate__fadeIn text-red-600 dark:text-red-200">
-              Connect your wallet first!
-            </span>
+      <h2 className="font-semibold mt-10 text-gray-900 dark:text-gray-100">
+        Stake
+      </h2>
+      <div className="flex flex-col mt-4 items-center gap-6">
+        <div className="flex gap-4">
+          <input
+            className="px-4 py-2 rounded-lg"
+            placeholder="Amount"
+            value={inputValue}
+            onChange={handleInputChange}
+          ></input>
+          {!confirmStaking && (
+            <button
+              className="flex border border-gray-900/10 dark:border-gray-100/10 bg-gray-50 dark:bg-gray-900 hover:shadow hover:border-gray-900/50 dark:hover:border-gray-100/50 focus:shadow-none focus:border-blue-600 dark:focus:border-blue-200 dark:hover-bg-blue-50 text-sm px-6 py-2 text-gray-900 dark:text-gray-100 rounded-lg font-semibold"
+              onClick={handleStake}
+            >
+              Stake
+            </button>
           )}
           {confirmStaking && (
-            <span className="animate__animated animate__fadeIn text-yellow-600 dark:text-yellow-200">
-              Your TrotelCoin will be locked for 30 days!
-            </span>
+            <button
+              className="flex border border-gray-900/10 dark:border-gray-100/10 bg-gray-50 dark:bg-gray-900 hover:shadow hover:border-gray-900/50 dark:hover:border-gray-100/50 focus:shadow-none focus:border-blue-600 dark:focus:border-blue-200 dark:hover-bg-blue-50 text-sm px-6 py-2 text-gray-900 dark:text-gray-100 rounded-lg font-semibold"
+              onClick={handleConfirm}
+            >
+              Confirm
+            </button>
           )}
         </div>
-        <h2 className="font-semibold mt-10 text-gray-900 dark:text-gray-100">
-          Staking dashboard
-        </h2>
-        <div className="flex flex-wrap mt-4 items-center gap-4 w-full">
-          <div className="flex w-full md:w-1/5 flex-col items-center justify-center gap-1 p-2 rounded-lg bg-gray-50 dark:bg-gray-900 border border-gray-900/10 dark:border-gray-100/10 hover:border-gray-900/50 dark:hover:border-gray-100/10 hover:shadow">
-            <h2 className="font-semibold text-6xl text-blue-600 dark:text-blue-200">
-              0
-            </h2>
-            <p className="text-center text-xs md:text-sm text-gray-900 dark:text-gray-100">
-              TrotelCoin
-            </p>
-          </div>
-          <div className="flex w-full md:w-1/5 flex-col items-center justify-center gap-1 p-2 rounded-lg bg-gray-50 dark:bg-gray-900 border border-gray-900/10 dark:border-gray-100/10 hover:border-gray-900/50 dark:hover:border-gray-100/10 hover:shadow">
-            <h2 className="font-semibold text-6xl text-blue-600 dark:text-blue-200">
-              {govBalanceData}
-            </h2>
-            <p className="text-center text-xs md:text-sm text-gray-900 dark:text-gray-100">
-              GovTrotelCoin
-            </p>
-          </div>
-          <div className="flex w-full md:w-1/5 flex-col items-center justify-center gap-1 p-2 rounded-lg bg-gray-50 dark:bg-gray-900 border border-gray-900/10 dark:border-gray-100/10 hover:border-gray-900/50 dark:hover:border-gray-100/10 hover:shadow">
-            <h2 className="font-semibold text-6xl text-blue-600 dark:text-blue-200">
-              0
-            </h2>
-            <p className="text-center text-xs md:text-sm text-gray-900 dark:text-gray-100">
-              Time until withdrawal
-            </p>
-          </div>
-          <div className="flex w-full md:w-1/5 flex-col items-center justify-center gap-1 p-2 rounded-lg bg-gray-50 dark:bg-gray-900 border border-gray-900/10 dark:border-gray-100/10 hover:border-gray-900/50 dark:hover:border-gray-100/10 hover:shadow">
-            <h2 className="font-semibold text-6xl text-blue-600 dark:text-blue-200">
-              {totalSupplyData}
-            </h2>
-            <p className="text-center text-xs md:text-sm text-gray-900 dark:text-gray-100">
-              GovTrotelCoin supply
-            </p>
-          </div>
+        {warningMessage !== "" && (
+          <span className="animate__animated animate__fadeIn text-red-600 dark:text-red-200">
+            Connect your wallet first!
+          </span>
+        )}
+        {confirmStaking && (
+          <span className="animate__animated animate__fadeIn text-yellow-600 dark:text-yellow-200">
+            Your TrotelCoin will be locked for 30 days!
+          </span>
+        )}
+      </div>
+      <h2 className="font-semibold mt-10 text-gray-900 dark:text-gray-100">
+        Staking dashboard
+      </h2>
+      <div className="flex flex-wrap mt-4 items-center gap-4 w-full">
+        <div className="flex w-full md:w-1/5 flex-col items-center justify-center gap-1 p-2 rounded-lg bg-gray-50 dark:bg-gray-900 border border-gray-900/10 dark:border-gray-100/10 hover:border-gray-900/50 dark:hover:border-gray-100/10 hover:shadow">
+          <h2 className="font-semibold text-6xl text-blue-600 dark:text-blue-200">
+            0
+          </h2>
+          <p className="text-center text-xs md:text-sm text-gray-900 dark:text-gray-100">
+            TrotelCoin
+          </p>
+        </div>
+        <div className="flex w-full md:w-1/5 flex-col items-center justify-center gap-1 p-2 rounded-lg bg-gray-50 dark:bg-gray-900 border border-gray-900/10 dark:border-gray-100/10 hover:border-gray-900/50 dark:hover:border-gray-100/10 hover:shadow">
+          <h2 className="font-semibold text-6xl text-blue-600 dark:text-blue-200">
+            {govBalanceData}
+          </h2>
+          <p className="text-center text-xs md:text-sm text-gray-900 dark:text-gray-100">
+            GovTrotelCoin
+          </p>
+        </div>
+        <div className="flex w-full md:w-1/5 flex-col items-center justify-center gap-1 p-2 rounded-lg bg-gray-50 dark:bg-gray-900 border border-gray-900/10 dark:border-gray-100/10 hover:border-gray-900/50 dark:hover:border-gray-100/10 hover:shadow">
+          <h2 className="font-semibold text-6xl text-blue-600 dark:text-blue-200">
+            0
+          </h2>
+          <p className="text-center text-xs md:text-sm text-gray-900 dark:text-gray-100">
+            Time until withdrawal
+          </p>
+        </div>
+        <div className="flex w-full md:w-1/5 flex-col items-center justify-center gap-1 p-2 rounded-lg bg-gray-50 dark:bg-gray-900 border border-gray-900/10 dark:border-gray-100/10 hover:border-gray-900/50 dark:hover:border-gray-100/10 hover:shadow">
+          <h2 className="font-semibold text-6xl text-blue-600 dark:text-blue-200">
+            {totalSupplyData}
+          </h2>
+          <p className="text-center text-xs md:text-sm text-gray-900 dark:text-gray-100">
+            GovTrotelCoin supply
+          </p>
         </div>
       </div>
     </>
