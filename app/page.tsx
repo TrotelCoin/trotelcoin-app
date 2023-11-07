cec81de3d6df75d72223471efd62d7461434d8d1
trotelalexandre <trotelalexandre@proton.me>
2023-11-07 10:37:59 +0100

    changing gradient

--- app/page.tsx
+++ app/page.tsx
@@ -255,8 +255,8 @@ export default function Home() {
                               </span>
                             )}
                             {course.new && (
-                              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-gradient-to-r from-yellow-200 dark:from-yellow-200 to-lime-200 dark:to-lime-200 text-gray-900 dark:text-gray-900">
-                                New
+                              <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-gradient-to-r from-indigo-500 dark:from-yellow-200 via-purple-500 dark:via-purple-200 to-pink-500 dark:to-pink-200 text-gray-100 dark:text-gray-900">
+                                New course
                               </span>
                             )}
                           </div>
