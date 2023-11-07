dc28522454e00ff2444557ec09c413b6d068d874
trotelalexandre <trotelalexandre@proton.me>
2023-11-07 18:46:00 +0100

    fix

--- app/governance/page.tsx
+++ app/governance/page.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import ComingSoon from "@/app/ui/interface/comingSoon";
 import { useAccount } from "wagmi";
 
 export default function Governance() {
+  const [isConnectedMessage, setIsConnectedMessage] = useState<string>("");
+
   const { address, isConnected } = useAccount();
+
+  useEffect(
+    const fetchStakingData = async () => {}
+  , []);
+
+  const handleStake = () => {
+    if (!isConnected) {
+      setIsConnectedMessage("Connect your wallet first!");
+    } else {
+      null;
+    }
+  };
 
   return (
     <>
@@ -18,10 +34,13 @@ export default function Governance() {
       </h2>
       <div className="flex mt-4 items-center gap-x-4">
         <input className="px-4 py-2 rounded-lg" placeholder="Amount"></input>
-        <button className="flex border border-gray-900/10 dark:border-gray-100/10 bg-gray-50 dark:bg-gray-900 hover:shadow hover:border-gray-900/50 dark:hover:border-gray-100/50 focus:shadow-none focus:border-blue-600 dark:focus:border-blue-200 dark:hover-bg-blue-50 text-sm px-6 py-2 text-gray-900 dark:text-gray-100 rounded-lg font-semibold">
+        <button
+          className="flex border border-gray-900/10 dark:border-gray-100/10 bg-gray-50 dark:bg-gray-900 hover:shadow hover:border-gray-900/50 dark:hover:border-gray-100/50 focus:shadow-none focus:border-blue-600 dark:focus:border-blue-200 dark:hover-bg-blue-50 text-sm px-6 py-2 text-gray-900 dark:text-gray-100 rounded-lg font-semibold"
+          onClick={handleStake}
+        >
           Stake
         </button>
-        {!isConnected && (
+        {isConnectedMessage && (
           <span className="mt-6 animate__animated animate__fadeIn text-red-600 dark:text-red-200">
             Connect your wallet first!
           </span>
