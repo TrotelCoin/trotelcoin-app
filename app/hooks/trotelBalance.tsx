0c9e88a1e330953a037b729f888222e8c13c5c39
Alexandre Trotel <trotelalexandre@proton.me>
2023-11-24 17:29:03 +0100

    fix

--- app/hooks/trotelBalance.tsx
+++ app/hooks/trotelBalance.tsx
@@ -1,36 +1,36 @@
 import { useBalance, useAccount } from "wagmi";
-import React from "react";
+import React, { useState } from "react";
 import { polygon } from "wagmi/chains";
 import { BalanceData } from "@/types/types";
 
 export default function TrotelBalance() {
+  const [balance, setBalance] = useState("0");
+
   // Get the Ethereum address using the getAccount function
   const { address } = useAccount();
 
-  try {
-    // Use the useBalance hook to fetch the balance data
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { data, isError, isLoading }: BalanceData = useBalance({
-      address: address as `0x${string}`, // Convert address to the correct format
-      token: "0x85057d5a8d063f9075Ba963101D76352051675E5", // Token address for TrotelCoin (TROTEL)
-      chainId: polygon.id,
-      watch: true,
-      enabled: true,
-    });
+  // Use the useBalance hook to fetch the balance data
+  const { data, isError, isLoading }: BalanceData = useBalance({
+    address: address as `0x${string}`, // Convert address to the correct format
+    token: "0x85057d5a8d063f9075Ba963101D76352051675E5", // Token address for TrotelCoin (TROTEL)
+    chainId: polygon.id,
+    watch: true,
+    enabled: true,
+  });
 
-    // If the data is still loading, return "0" within <></> (JSX fragment)
-    if (isLoading) return <span className="animate-pulse">0 </span>;
-
-    // If there is an error, return "0" within <></> (JSX fragment)
-    if (isError) return <span className="animate-pulse">0 </span>;
-
-    // Extract the balance value from the formatted data or default to "0"
-    let balance = (data?.formatted as string) ?? "0";
-
-    // Parse the balance value to a whole number, format it as a string, and wrap it within <></> (JSX fragment)
-    return <>{parseFloat(balance).toFixed(0)}</>;
-  } catch (error) {
-    console.log("An error occurred", error); // Log any errors that occur
-    return <span className="animate-pulse">0 </span>; // Return "0" with a CSS animation (pulse)
+  // If the data is still loading, return "0" within <></> (JSX fragment)
+  if (isLoading) {
+    setBalance("0");
   }
+
+  // If there is an error, return "0" within <></> (JSX fragment)
+  if (isError) {
+    setBalance("0");
+  }
+
+  // Extract the balance value from the formatted data or default to "0"
+  if (data?.formatted) setBalance(data?.formatted);
+
+  // Parse the balance value to a whole number, format it as a string, and wrap it within <></> (JSX fragment)
+  return <>{parseFloat(balance).toFixed(0)}</>;
 }
