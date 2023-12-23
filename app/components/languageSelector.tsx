540508a67af73473c89a4b44567bb5754d16a431
Alexandre Trotel <trotelalexandre@proton.me>
2023-12-23 23:08:06 +0100

    fix

--- app/components/languageSelector.tsx
+++ app/components/languageSelector.tsx
@@ -41,7 +41,7 @@ const LanguageSelector = () => {
                     className={`${
                       active
                         ? "bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100"
-                        : "text-gray-700 dark:text-gray-300"
+                        : "text-gray-900 dark:text-gray-100"
                     } hover:bg-gray-100 dark:hover:bg-gray-800 block px-4 py-2 text-sm w-full text-left rounded-lg`}
                   >
                     {language.label}
