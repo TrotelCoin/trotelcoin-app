c1294b141568e9366d52de3d153d5114f793c835
Alexandre Trotel <trotelalexandre@proton.me>
2023-12-26 15:23:26 +0100

    fic

--- app/ui/modals/fail.tsx
+++ app/ui/modals/fail.tsx
@@ -56,7 +56,7 @@ const Fail: React.FC<Modals> = ({ title, show, message, onClose }) => {
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-full bg-blue-200 dark:hover:bg-blue-200/80 hover:bg-blue-200/80 dark:bg-blue-200 dark:hover-bg-blue-50 px-3 py-2 text-sm font-semibold text-gray-900"
+                    className="inline-flex w-full justify-center rounded-full bg-blue-600 dark:hover:bg-blue-300 hover:bg-blue-800 dark:bg-blue-200 px-3 py-2 text-sm font-semibold text-gray-900"
                     onClick={onClose}
                   >
                     Close
