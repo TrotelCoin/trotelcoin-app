2d97526a4fc4b503ee749c05c65a81f386c792e1
Alexandre Trotel <trotelalexandre@proton.me>
2023-12-29 00:26:13 +0100

    fix

--- app/[lang]/ui/interface/loading.tsx
+++ app/[lang]/ui/interface/loading.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
 
 const Loading = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 animate-spin"></div>
+      <div className="animate-bounce">
+        <Image
+          width={128}
+          height={128}
+          alt="TrotelCoin Logo"
+          src="/assets/logo/trotelcoin-white.png"
+        ></Image>
+      </div>
     </div>
   );
 };
