20b2f4f58347b2a4950e5feee8e239aeb9151c61
Alexandre Trotel <trotelalexandre@proton.me>
2023-12-29 01:08:41 +0100

    fix

--- app/[lang]/ui/interface/loading.tsx
+++ app/[lang]/ui/interface/loading.tsx
@@ -1,22 +1,9 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 
 const Loading = () => {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-bounce">
-        <Image
-          width={128}
-          height={128}
-          alt="TrotelCoin Logo"
-          className="w-12 h-auto animate-pulse"
-          src="/assets/logo/trotelcoin-white.png"
-        ></Image>
-      </div>
-    </div>
-  );
+  return <div className="flex justify-center items-center h-screen"></div>;
 };
 
 export default Loading;
