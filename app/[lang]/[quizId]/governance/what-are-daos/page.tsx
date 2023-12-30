d47fce06f982c48c83f536c0aa4ebd0ef8252d45
Alexandre Trotel <trotelalexandre@proton.me>
2023-12-30 16:25:39 +0100

    factorization

--- app/[lang]/[quizId]/governance/what-are-daos/page.tsx
+++ app/[lang]/[quizId]/governance/what-are-daos/page.tsx
@@ -1,258 +1,65 @@
 "use client";
 
-import "animate.css";
-import { Course, DictType, Lang } from "@/types/types";
-import React, { useEffect, useState } from "react";
-import lessons from "@/data/lessonsData";
-import Link from "next/link";
-import Quiz from "@/app/[lang]/components/quiz";
-import { useAccount, useContractRead } from "wagmi";
-import GoHomeButton from "@/app/[lang]/components/goHomeButton";
-import trotelCoinExpertABI from "@/abi/trotelCoinExpert";
-import trotelCoinIntermediateABI from "@/abi/trotelCoinIntermediate";
-import {
-  trotelCoinIntermediateAddress,
-  trotelCoinExpertAddress,
-} from "@/data/addresses";
-import { polygon } from "viem/chains";
-import { getDictionary } from "@/app/[lang]/dictionaries";
+import React from "react";
 
-const getTierByQuizId = (quizIdParam: number): string => {
-  let foundTier = "";
-  lessons.forEach((lesson) => {
-    lesson.courses.forEach((course) => {
-      if (course.quizId.toString() === quizIdParam.toString()) {
-        foundTier = course.tier;
-      }
-    });
-  });
-  return foundTier;
-};
-
-const getAvailabilityByQuizId = (quizIdParam: number): boolean => {
-  let foundAvailability = false;
-  lessons.forEach((lesson) => {
-    lesson.courses.forEach((course) => {
-      if (course.quizId.toString() === quizIdParam.toString()) {
-        foundAvailability = course.available;
-      }
-    });
-  });
-  return foundAvailability;
-};
-
-const CoursePage = ({
-  params: { lang, quizId },
-}: {
-  params: { lang: Lang; quizId: number };
-}) => {
-  const [dict, setDict] = useState<DictType | null>(null);
-
-  useEffect(() => {
-    const fetchDictionary = async () => {
-      const result = await getDictionary(lang);
-      setDict(result);
-    };
-
-    fetchDictionary();
-  }, [lang]);
-
-  const tier = getTierByQuizId(quizId);
-  const available = getAvailabilityByQuizId(quizId);
-
-  const currentCourse: Course = lessons
-    .flatMap((lesson) => lesson.courses)
-    .find((course) => course.quizId.toString() === quizId.toString()) as Course;
-
-  const { address, isDisconnected } = useAccount();
-
-  const { data: intermediate } = useContractRead({
-    chainId: polygon.id,
-    address: trotelCoinIntermediateAddress,
-    abi: trotelCoinIntermediateABI,
-    args: [address],
-    account: address,
-    enabled: Boolean(address),
-    functionName: "balanceOf",
-    watch: true,
-  });
-  const { data: expert } = useContractRead({
-    chainId: polygon.id,
-    address: trotelCoinExpertAddress,
-    abi: trotelCoinExpertABI,
-    args: [address],
-    account: address,
-    enabled: Boolean(address),
-    functionName: "balanceOf",
-    watch: true,
-  });
-
-  const intermediateBalance = parseFloat(intermediate as string);
-  const expertBalance = parseFloat(expert as string);
-
-  const renderUnauthorizedContent = () => {
-    return (
-      <>
-        <main className="grid min-h-full place-items-center bg-white dark:bg-black px-6 py-24 sm:py-32 lg:px-8">
-          <div className="text-center">
-            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl">
-              {typeof dict?.lesson !== "string" && (
-                <>{dict?.lesson.notAvailable}</>
-              )}
-            </h1>
-            <p className="mt-6 text-base leading-7 text-gray-700 dark:text-gray-300">
-              {typeof dict?.lesson !== "string" && (
-                <>{dict?.lesson.notAvailableMessage}</>
-              )}
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <Link
-                href={`/${lang}/home`}
-                className="rounded-md bg-blue-600 dark:bg-blue-200 px-3.5 py-2.5 text-sm font-semibold text-gray-100 dark:text-gray-900 shadow-sm hover:bg-blue-800 dark:hover:bg-blue-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-              >
-                {typeof dict?.lesson !== "string" && (
-                  <>{dict?.lesson.goBackHome}</>
-                )}
-              </Link>
-            </div>
+const CoursePage = () => {
+  return (
+    <>
+      <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
+        Install your first wallet
+      </h2>
+      <p className="mt-6">
+        Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in.
+        Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris
+        enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus
+        mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
+        Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
+        diam.
+      </p>
+      <figure className="mt-10 border-l border-blue-600 dark:border-blue-200 pl-9">
+        <blockquote className="font-semibold text-gray-900 dark:text-gray-100">
+          <p>
+            “Vel ultricies morbi odio facilisi ultrices accumsan donec lacus
+            purus. Lectus nibh ullamcorper ac dictum justo in euismod. Risus
+            aenean ut elit massa. In amet aliquet eget cras. Sem volutpat enim
+            tristique.”
+          </p>
+        </blockquote>
+        <figcaption className="mt-6 flex gap-x-4">
+          <div className="text-sm leading-6">
+            <strong className="font-semibold text-gray-900 dark:text-gray-100">
+              Alexandre Trotel
+            </strong>{" "}
+            – CEO & Founder
           </div>
-        </main>
-      </>
-    );
-  };
+        </figcaption>
+      </figure>
+      <p className="mt-10">
+        Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim.
+        Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent
+        donec est. Odio penatibus risus viverra tellus varius sit neque erat
+        velit.
+      </p>
 
-  const renderCourseContent = () => {
-    return (
-      <>
-        <p className="text-base font-semibold leading-7 text-blue-600 dark:text-blue-200">
-          {typeof dict?.lesson !== "string" && <>{dict?.lesson.course}</>}
-        </p>
-        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">
-          {currentCourse?.title}
-        </h1>
-        <p className="mt-2 text-gray-900 dark:text-gray-100">
-          {typeof dict?.lesson !== "string" && <>{dict?.lesson.goingToLearn}</>}
-        </p>
-        <div className="bg-gray-50 my-10 border backdrop-blur-xl border-gray-900/10 dark:border-gray-100/10 rounded-lg px-10 py-2 dark:bg-gray-900">
-          <ul
-            role="list"
-            className="max-w-xl space-y-8 text-gray-700 dark:text-gray-300"
-          >
-            <div className="grid grid-cols-1 divide-y divide-gray-900/10 dark:divide-gray-100/10">
-              <div className="py-4">
-                <li className="flex gap-x-3">
-                  <span className="text-gray-900 dark:text-gray-100">
-                    {typeof dict?.lesson !== "string" &&
-                      typeof dict?.lesson.lessons !== "string" &&
-                      typeof dict?.lesson.lessons.introductionToTrotelCoin !==
-                        "string" && (
-                        <>{dict?.lesson.lessons.introductionToTrotelCoin.one}</>
-                      )}
-                  </span>
-                </li>
-              </div>
-              <div className="py-4">
-                <li className="flex gap-x-3">
-                  <span className="text-gray-900 dark:text-gray-100">
-                    {typeof dict?.lesson !== "string" &&
-                      typeof dict?.lesson.lessons !== "string" &&
-                      typeof dict?.lesson.lessons.introductionToTrotelCoin !==
-                        "string" && (
-                        <>{dict?.lesson.lessons.introductionToTrotelCoin.two}</>
-                      )}
-                  </span>
-                </li>
-              </div>
-              <div className="py-4">
-                <li className="flex gap-x-3">
-                  <span className="text-gray-900 dark:text-gray-100">
-                    {typeof dict?.lesson !== "string" &&
-                      typeof dict?.lesson.lessons !== "string" &&
-                      typeof dict?.lesson.lessons.introductionToTrotelCoin !==
-                        "string" && (
-                        <>
-                          {dict?.lesson.lessons.introductionToTrotelCoin.three}
-                        </>
-                      )}
-                  </span>
-                </li>
-              </div>
-            </div>
-          </ul>
-        </div>
+      <div className="mt-16 mx-auto">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
-          Install your first wallet
+          Configure your wallet
         </h2>
         <p className="mt-6">
-          Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat
-          in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum
-          mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-          tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
-          Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
-          diam.
+          Purus morbi dignissim senectus mattis adipiscing. Amet, massa quam
+          varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales
+          cursus tristique. Tincidunt sed tempus ut viverra ridiculus non
+          molestie. Gravida quis fringilla amet eget dui tempor dignissim.
+          Facilisis auctor venenatis varius nunc, congue erat ac. Cras fermentum
+          convallis quam.
         </p>
-        <figure className="mt-10 border-l border-blue-600 dark:border-blue-200 pl-9">
-          <blockquote className="font-semibold text-gray-900 dark:text-gray-100">
-            <p>
-              “Vel ultricies morbi odio facilisi ultrices accumsan donec lacus
-              purus. Lectus nibh ullamcorper ac dictum justo in euismod. Risus
-              aenean ut elit massa. In amet aliquet eget cras. Sem volutpat enim
-              tristique.”
-            </p>
-          </blockquote>
-          <figcaption className="mt-6 flex gap-x-4">
-            <div className="text-sm leading-6">
-              <strong className="font-semibold text-gray-900 dark:text-gray-100">
-                Alexandre Trotel
-              </strong>{" "}
-              – CEO & Founder
-            </div>
-          </figcaption>
-        </figure>
-        <p className="mt-10">
+        <p className="mt-8">
           Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
           enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
           praesent donec est. Odio penatibus risus viverra tellus varius sit
           neque erat velit.
         </p>
-
-        <div className="mt-16 mx-auto">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
-            Configure your wallet
-          </h2>
-          <p className="mt-6">
-            Purus morbi dignissim senectus mattis adipiscing. Amet, massa quam
-            varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales
-            cursus tristique. Tincidunt sed tempus ut viverra ridiculus non
-            molestie. Gravida quis fringilla amet eget dui tempor dignissim.
-            Facilisis auctor venenatis varius nunc, congue erat ac. Cras
-            fermentum convallis quam.
-          </p>
-          <p className="mt-8">
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit.
-          </p>
-        </div>
-
-        {/* Quizz */}
-        <Quiz quizId={quizId} lang={lang} />
-        <GoHomeButton lang={lang} />
-      </>
-    );
-  };
-
-  return (
-    <>
-      {isDisconnected && tier !== "Beginner"
-        ? renderUnauthorizedContent()
-        : !available ||
-          (tier !== "Beginner" &&
-            ((tier === "Intermediate" && intermediateBalance < 1) ||
-              (tier === "Expert" && expertBalance < 1)))
-        ? renderUnauthorizedContent()
-        : renderCourseContent()}
+      </div>
     </>
   );
 };
--- app/[lang]/[quizId]/layout.tsx
+++ app/[lang]/[quizId]/layout.tsx
@@ -1,13 +1,219 @@
 "use client";
 
-import { Lang } from "@/types/types";
-import React from "react";
+import "animate.css";
+import { Course, DictType, Lang } from "@/types/types";
+import React, { useEffect, useState } from "react";
+import lessons from "@/data/lessonsData";
+import Link from "next/link";
+import Quiz from "@/app/[lang]/components/quiz";
+import { useAccount, useContractRead } from "wagmi";
+import GoHomeButton from "@/app/[lang]/components/goHomeButton";
+import trotelCoinExpertABI from "@/abi/trotelCoinExpert";
+import trotelCoinIntermediateABI from "@/abi/trotelCoinIntermediate";
+import {
+  trotelCoinIntermediateAddress,
+  trotelCoinExpertAddress,
+} from "@/data/addresses";
+import { polygon } from "viem/chains";
+import { getDictionary } from "@/app/[lang]/dictionaries";
 
-// Define the main Document component
-export default function Layout({ children }: { children: React.ReactNode }) {
+const getTierByQuizId = (quizIdParam: number): string => {
+  let foundTier = "";
+  lessons.forEach((lesson) => {
+    lesson.courses.forEach((course) => {
+      if (course.quizId.toString() === quizIdParam.toString()) {
+        foundTier = course.tier;
+      }
+    });
+  });
+  return foundTier;
+};
+
+const getAvailabilityByQuizId = (quizIdParam: number): boolean => {
+  let foundAvailability = false;
+  lessons.forEach((lesson) => {
+    lesson.courses.forEach((course) => {
+      if (course.quizId.toString() === quizIdParam.toString()) {
+        foundAvailability = course.available;
+      }
+    });
+  });
+  return foundAvailability;
+};
+
+const CoursePage = ({
+  params: { lang, quizId },
+  children,
+}: {
+  params: { lang: Lang; quizId: number };
+  children: React.ReactNode;
+}) => {
+  const [dict, setDict] = useState<DictType | null>(null);
+
+  useEffect(() => {
+    const fetchDictionary = async () => {
+      const result = await getDictionary(lang);
+      setDict(result);
+    };
+
+    fetchDictionary();
+  }, [lang]);
+
+  const tier = getTierByQuizId(quizId);
+  const available = getAvailabilityByQuizId(quizId);
+
+  const currentCourse: Course = lessons
+    .flatMap((lesson) => lesson.courses)
+    .find((course) => course.quizId.toString() === quizId.toString()) as Course;
+
+  const { address, isDisconnected } = useAccount();
+
+  const { data: intermediate } = useContractRead({
+    chainId: polygon.id,
+    address: trotelCoinIntermediateAddress,
+    abi: trotelCoinIntermediateABI,
+    args: [address],
+    account: address,
+    enabled: Boolean(address),
+    functionName: "balanceOf",
+    watch: true,
+  });
+  const { data: expert } = useContractRead({
+    chainId: polygon.id,
+    address: trotelCoinExpertAddress,
+    abi: trotelCoinExpertABI,
+    args: [address],
+    account: address,
+    enabled: Boolean(address),
+    functionName: "balanceOf",
+    watch: true,
+  });
+
+  const intermediateBalance = parseFloat(intermediate as string);
+  const expertBalance = parseFloat(expert as string);
+
+  const renderUnauthorizedContent = () => {
+    return (
+      <>
+        <main className="grid min-h-full place-items-center bg-white dark:bg-black px-6 py-24 sm:py-32 lg:px-8">
+          <div className="text-center">
+            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl">
+              {typeof dict?.lesson !== "string" && (
+                <>{dict?.lesson.notAvailable}</>
+              )}
+            </h1>
+            <p className="mt-6 text-base leading-7 text-gray-700 dark:text-gray-300">
+              {typeof dict?.lesson !== "string" && (
+                <>{dict?.lesson.notAvailableMessage}</>
+              )}
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <Link
+                href={`/${lang}/home`}
+                className="rounded-md bg-blue-600 dark:bg-blue-200 px-3.5 py-2.5 text-sm font-semibold text-gray-100 dark:text-gray-900 shadow-sm hover:bg-blue-800 dark:hover:bg-blue-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+              >
+                {typeof dict?.lesson !== "string" && (
+                  <>{dict?.lesson.goBackHome}</>
+                )}
+              </Link>
+            </div>
+          </div>
+        </main>
+      </>
+    );
+  };
+
+  const renderCourseContent = (children: React.ReactNode) => {
+    return (
+      <>
+        <div className="mx-auto max-w-2xl text-base leading-7 text-gray-900 dark:text-gray-100">
+          <p className="text-base font-semibold leading-7 text-blue-600 dark:text-blue-200">
+            {typeof dict?.lesson !== "string" && <>{dict?.lesson.course}</>}
+          </p>
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">
+            {currentCourse?.title}
+          </h1>
+          <p className="mt-2 text-gray-900 dark:text-gray-100">
+            {typeof dict?.lesson !== "string" && (
+              <>{dict?.lesson.goingToLearn}</>
+            )}
+          </p>
+          <div className="bg-gray-50 my-10 border backdrop-blur-xl border-gray-900/10 dark:border-gray-100/10 rounded-lg px-10 py-2 dark:bg-gray-900">
+            <ul
+              role="list"
+              className="max-w-xl space-y-8 text-gray-700 dark:text-gray-300"
+            >
+              <div className="grid grid-cols-1 divide-y divide-gray-900/10 dark:divide-gray-100/10">
+                <div className="py-4">
+                  <li className="flex gap-x-3">
+                    <span className="text-gray-900 dark:text-gray-100">
+                      {typeof dict?.lesson !== "string" &&
+                        typeof dict?.lesson.lessons !== "string" &&
+                        typeof dict?.lesson.lessons.introductionToTrotelCoin !==
+                          "string" && (
+                          <>
+                            {dict?.lesson.lessons.introductionToTrotelCoin.one}
+                          </>
+                        )}
+                    </span>
+                  </li>
+                </div>
+                <div className="py-4">
+                  <li className="flex gap-x-3">
+                    <span className="text-gray-900 dark:text-gray-100">
+                      {typeof dict?.lesson !== "string" &&
+                        typeof dict?.lesson.lessons !== "string" &&
+                        typeof dict?.lesson.lessons.introductionToTrotelCoin !==
+                          "string" && (
+                          <>
+                            {dict?.lesson.lessons.introductionToTrotelCoin.two}
+                          </>
+                        )}
+                    </span>
+                  </li>
+                </div>
+                <div className="py-4">
+                  <li className="flex gap-x-3">
+                    <span className="text-gray-900 dark:text-gray-100">
+                      {typeof dict?.lesson !== "string" &&
+                        typeof dict?.lesson.lessons !== "string" &&
+                        typeof dict?.lesson.lessons.introductionToTrotelCoin !==
+                          "string" && (
+                          <>
+                            {
+                              dict?.lesson.lessons.introductionToTrotelCoin
+                                .three
+                            }
+                          </>
+                        )}
+                    </span>
+                  </li>
+                </div>
+              </div>
+            </ul>
+          </div>
+          <>{children}</>
+
+          {/* Quizz */}
+          <Quiz quizId={quizId} lang={lang} />
+          <GoHomeButton lang={lang} />
+        </div>
+      </>
+    );
+  };
+
   return (
-    <div className="mx-auto max-w-2xl text-base leading-7 text-gray-700 dark:text-gray-300">
-      {children}
-    </div>
+    <>
+      {isDisconnected && tier !== "Beginner"
+        ? renderUnauthorizedContent()
+        : !available ||
+          (tier !== "Beginner" &&
+            ((tier === "Intermediate" && intermediateBalance < 1) ||
+              (tier === "Expert" && expertBalance < 1)))
+        ? renderUnauthorizedContent()
+        : renderCourseContent(children)}
+    </>
   );
-}
+};
+
+export default CoursePage;
