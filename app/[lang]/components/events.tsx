b1b642c4500fee8fcaf5a598f323cb24c5a5ae7f
Alexandre Trotel <trotelalexandre@proton.me>
2023-12-31 16:08:40 +0100

    adding ENS name

--- app/[lang]/components/events.tsx
+++ app/[lang]/components/events.tsx
@@ -3,8 +3,8 @@ import { trotelCoinLearningAddress } from "@/data/addresses";
 import { DictType, Lang } from "@/types/types";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import React, { useEffect, useState } from "react";
-import { polygon } from "viem/chains";
-import { useContractEvent } from "wagmi";
+import { mainnet, polygon } from "viem/chains";
+import { useContractEvent, useEnsName } from "wagmi";
 import { Address, Log } from "viem";
 import { getDictionary } from "@/app/[lang]/dictionaries";
 import { reduceAddressSize } from "@/utils/utils";
@@ -18,10 +18,11 @@ type MyLog = Log & {
 };
 
 export default function Events({ lang }: { lang: Lang }) {
-  const [rewardsClaimed, setRewardsClaimed] = useState<Log[] | null>(null);
-  const [user, setUser] = useState<Address | null>(null);
+  const [user, setUser] = useState<string | null>(null);
   const [amountClaimed, setAmountClaimed] = useState<number | null>(null);
   const [dict, setDict] = useState<DictType | null>(null);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [isMounted, setIsMounted] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchDictionary = async () => {
@@ -38,8 +39,7 @@ export default function Events({ lang }: { lang: Lang }) {
     abi: trotelCoinLearningABI,
     eventName: "RewardsClaimed",
     listener(log) {
-      setRewardsClaimed(log);
-      console.log(log);
+      setIsVisible(true);
 
       const length = log.length;
 
@@ -50,66 +50,90 @@ export default function Events({ lang }: { lang: Lang }) {
       );
 
       setTimeout(() => {
-        setRewardsClaimed(null);
+        setIsVisible(false);
       }, 10000);
     },
   });
 
+  const { data: ensName, isSuccess: ensNameExists } = useEnsName({
+    chainId: mainnet.id,
+    address: user as Address,
+    enabled: Boolean(user),
+  });
+
+  useEffect(() => {
+    if (ensNameExists) {
+      setUser(ensName as string);
+    }
+  }, [ensNameExists]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsMounted(true);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       <div
-        className={`${
-          rewardsClaimed ? "fixed" : "hidden"
-        } inset-x-0 bottom-0 pb-2 sm:pb-5 animate__animated animate__fadeInUp z-50`}
+        className={`fixed inset-x-0 bottom-0 pb-2 sm:pb-5 ${
+          isVisible
+            ? "animate__animated animate__fadeInUp"
+            : "animate__animated animate__fadeOutDown"
+        } animate__faster z-50`}
       >
-        <div className="mx-auto max-w-4xl px-2 sm:px-6 lg:px-8">
-          <div className="rounded-lg bg-green-600 dark:bg-green-200 p-2 shadow-lg sm:p-3">
-            <div className="flex flex-wrap items-center justify-between">
-              <div className="flex w-0 flex-1 items-center">
-                <span className="flex rounded-lg bg-green-800 dark:bg-green-300 p-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6 text-gray-100 dark:text-gray-900"
+        <div className={`${!isMounted && "hidden"}`}>
+          <div className="mx-auto max-w-4xl px-2 sm:px-6 lg:px-8">
+            <div className="rounded-lg bg-green-600 dark:bg-green-200 p-2 shadow-lg sm:p-3">
+              <div className="flex flex-wrap items-center justify-between">
+                <div className="flex w-0 flex-1 items-center">
+                  <span className="flex rounded-lg bg-green-800 dark:bg-green-300 p-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 text-gray-100 dark:text-gray-900"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+                      />
+                    </svg>
+                  </span>
+                  <p className="ml-3 truncate font-medium text-gray-100 dark:text-gray-900">
+                    <span className="md:hidden">
+                      {user as Address}{" "}
+                      {typeof dict?.events !== "string" && (
+                        <>{dict?.events.claimed}</>
+                      )}{" "}
+                      {amountClaimed as number} TrotelCoins!
+                    </span>
+                    <span className="hidden md:inline">
+                      {user as Address}{" "}
+                      {typeof dict?.events !== "string" && (
+                        <>{dict?.events.claimed}</>
+                      )}{" "}
+                      {amountClaimed as number} TrotelCoins!
+                    </span>
+                  </p>
+                </div>
+                <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
+                  <button
+                    type="button"
+                    className="-mr-1 flex rounded-md p-2 hover:bg-green-800 dark:hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-900"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+                    <span className="sr-only">Dismiss</span>
+                    <XMarkIcon
+                      className="h-6 w-6 text-gray-100 dark:text-gray-900"
+                      aria-hidden="true"
                     />
-                  </svg>
-                </span>
-                <p className="ml-3 truncate font-medium text-gray-100 dark:text-gray-900">
-                  <span className="md:hidden">
-                    {user as Address}{" "}
-                    {typeof dict?.events !== "string" && (
-                      <>{dict?.events.claimed}</>
-                    )}{" "}
-                    {amountClaimed as number} TrotelCoins!
-                  </span>
-                  <span className="hidden md:inline">
-                    {user as Address}{" "}
-                    {typeof dict?.events !== "string" && (
-                      <>{dict?.events.claimed}</>
-                    )}{" "}
-                    {amountClaimed as number} TrotelCoins!
-                  </span>
-                </p>
-              </div>
-              <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
-                <button
-                  type="button"
-                  className="-mr-1 flex rounded-md p-2 hover:bg-green-800 dark:hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-900"
-                >
-                  <span className="sr-only">Dismiss</span>
-                  <XMarkIcon
-                    className="h-6 w-6 text-gray-100 dark:text-gray-900"
-                    aria-hidden="true"
-                  />
-                </button>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
