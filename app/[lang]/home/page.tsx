384ef458cd1ae9f2a954580d279641478baa170a
Alexandre Trotel <trotelalexandre@proton.me>
2024-01-01 22:14:04 +0100

    fix

--- app/[lang]/home/page.tsx
+++ app/[lang]/home/page.tsx
@@ -267,7 +267,7 @@ export default function Home({ params: { lang } }: { params: { lang: Lang } }) {
               <h2 className="font-semibold text-xl text-gray-900 dark:text-gray-100">
                 {lesson.category}
               </h2>
-              <div className="mt-1 grid grid-cols-1 lg:grid-cols-2 lg:grid-cols-3">
+              <div className="mt-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                 {lesson.courses
                   .slice()
                   .sort((a, b) => {
