eb253c1a5f52eb3fbba03fd9a74dd18166899ed5
Alexandre Trotel <trotelalexandre@proton.me>
2024-01-31 01:44:07 +0100

    fix

--- lib/calculateRewards.ts
+++ lib/calculateRewards.ts
@@ -1,5 +1,5 @@
 export function calculateRewards(remainingRewards: number): number {
-    const minReward = remainingRewards / 10;
-    const maxReward = remainingRewards / 4;
-    return Math.floor(Math.random() * (maxReward - minReward + 1)) + minReward;
+  const minReward = remainingRewards / 10;
+  const maxReward = remainingRewards / 4;
+  return Math.floor(Math.random() * (maxReward - minReward + 1)) + minReward;
 }
