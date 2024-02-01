115c349253db2488c2b5ddaa136bf57dadd26113
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-01 02:40:19 +0100

    timestamp for quizzes answered

--- pages/api/database/updateRewards.ts
+++ pages/api/database/updateRewards.ts
@@ -25,7 +25,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         await sql`SELECT remaining_rewards FROM "algorithm"`;
       const rewards = calculateRewards(remainingRewards[0].remaining_rewards);
       await sql`INSERT INTO "learners" (wallet, total_rewards_pending) VALUES (${wallet}, ${rewards}) ON CONFLICT (wallet) DO UPDATE SET total_rewards_pending = "learners".total_rewards_pending + ${rewards}`;
-      await sql`INSERT INTO "quizzes_answered" (wallet, quiz_id, answered) VALUES (${wallet}, ${quizId}, true) ON CONFLICT (id) DO UPDATE SET answered = true`;
+      await sql`INSERT INTO "quizzes_answered" (wallet, quiz_id, answered, answered_at) VALUES (${wallet}, ${quizId}, true, now()) ON CONFLICT (id) DO UPDATE SET answered = true`;
       await sql`ALTER TABLE "algorithm" REPLICA IDENTITY FULL`;
       await sql`UPDATE "algorithm" SET remaining_rewards = remaining_rewards - ${
         rewards / 50
