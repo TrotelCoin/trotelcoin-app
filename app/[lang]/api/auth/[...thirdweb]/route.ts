494732d6881cf9effffc1312fa9cd7fdf88f647d
Alexandre Trotel <113524174+trotelalexandre@users.noreply.github.com>
2024-02-06 15:33:32 +0100

    Delete app/[lang]/api directory

--- app/[lang]/api/auth/[...thirdweb]/route.ts
+++
@@ -1,9 +0,0 @@
-import { ThirdwebAuthAppRouter } from "@thirdweb-dev/auth/next";
-import { PrivateKeyWallet } from "@thirdweb-dev/auth/evm";
-
-export const { ThirdwebAuthHandler, getUser } = ThirdwebAuthAppRouter({
-  domain: process.env.NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN ?? "",
-  wallet: new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY ?? ""),
-});
-
-export { ThirdwebAuthHandler as GET, ThirdwebAuthHandler as POST };
