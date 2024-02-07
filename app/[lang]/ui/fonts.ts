dddafa28cadf18c76d20fe676e4c93a32c6fcb1c
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-07 18:10:03 +0100

    changing font to inter

--- app/[lang]/ui/fonts.ts
+++ app/[lang]/ui/fonts.ts
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 
 export const inter = Inter({
-  subsets: ["devanagari"],
+  subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
