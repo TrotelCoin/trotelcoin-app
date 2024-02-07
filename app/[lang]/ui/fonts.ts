36a9fa0cfba846a0089a6ce64dbf816564182e3d
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-07 19:33:40 +0100

    fix

--- app/[lang]/ui/fonts.ts
+++ app/[lang]/ui/fonts.ts
@@ -1,6 +1,6 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-export const inter = Inter({
-  subsets: ["latin"],
+export const poppins = Poppins({
+  subsets: ["devanagari"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
