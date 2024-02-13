019126445ca92b47c76818ad765fb01aebc2ba74
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-13 18:43:06 +0100

    fix: header adding modals on mobile

--- app/[lang]/ui/interface/header.tsx
+++ app/[lang]/ui/interface/header.tsx
@@ -349,7 +349,11 @@ const Header = ({
         <div className="flex gap-2 items-center lg:hidden">
           <div className="flex items-center">
             <div className="flex gap-2 items-center">
-              <div className="flex gap-1 text-xl items-center text-gray-900 dark:text-gray-100">
+              <div
+                className="relative flex justify-center gap-1 text-xl items-center text-gray-900 dark:text-gray-100 cursor-pointer"
+                onMouseEnter={() => setIsHoveringLife(true)}
+                onMouseLeave={() => setIsHoveringLife(false)}
+              >
                 {isExpertBalance || isIntermediateBalance ? (
                   <span className="font-semibold text-2xl">&infin;</span>
                 ) : life ? (
@@ -360,8 +364,42 @@ const Header = ({
                   <span className="font-semibold">0</span>
                 )}{" "}
                 💛
+                <Transition
+                  as={Fragment}
+                  show={isHoveringLife}
+                  enter="transition ease-out duration-200"
+                  enterFrom="opacity-0 translate-y-1"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition ease-in duration-150"
+                  leaveFrom="opacity-100 translate-y-0"
+                  leaveTo="opacity-0 translate-y-1"
+                >
+                  <div
+                    className="absolute flex justify-center bg-white dark:bg-gray-900 z-10 mt-3 text-sm text-gray-900 dark:text-gray-100"
+                    style={{ width: "300px" }}
+                  >
+                    <div className="absolute flex flex-col gap-4 bg-white dark:bg-gray-900 justify-center items-center top-5 z-50 border border-gray-900/20 dark:border-gray-100/40 p-4 rounded-xl">
+                      <p>
+                        {typeof dict?.header !== "string" && (
+                          <>{dict?.header.lifeMessage}</>
+                        )}
+                      </p>
+                      <Link href={`/${lang}/premium`}>
+                        <button className="bg-yellow-500 hover:bg-yellow-400 dark:bg-yellow-300 dark:hover:bg-yellow-400 hover:border-gray-900/50 dark:hover:border-gray-100/50 focus:border-yellow-500 dark:focus:border-yellow-300 text-sm px-6 py-2 text-gray-900 dark:text-gray-900 rounded-lg font-semibold">
+                          {typeof dict?.header !== "string" && (
+                            <>{dict?.header.lifeButton}</>
+                          )}
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </Transition>
               </div>
-              <div className="flex gap-1 text-xl items-center text-gray-900 dark:text-gray-100">
+              <div
+                className="relative flex justify-center gap-1 text-xl items-center text-gray-900 dark:text-gray-100 cursor-pointer"
+                onMouseEnter={() => setIsHoveringStreak(true)}
+                onMouseLeave={() => setIsHoveringStreak(false)}
+              >
                 {streak ? (
                   <span className="font-semibold">
                     <CountUp start={0} end={streak} duration={2} />
@@ -370,6 +408,36 @@ const Header = ({
                   <span className="font-semibold">0</span>
                 )}{" "}
                 🔥
+                <Transition
+                  as={Fragment}
+                  show={isHoveringStreak}
+                  enter="transition ease-out duration-200"
+                  enterFrom="opacity-0 translate-y-1"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition ease-in duration-150"
+                  leaveFrom="opacity-100 translate-y-0"
+                  leaveTo="opacity-0 translate-y-1"
+                >
+                  <div
+                    className="absolute flex justify-center bg-white dark:bg-gray-900 z-10 mt-3 text-sm text-gray-900 dark:text-gray-100"
+                    style={{ width: "300px" }}
+                  >
+                    <div className="absolute flex flex-col gap-4 bg-white dark:bg-gray-900 justify-center items-center top-5 z-50 border border-gray-900/20 dark:border-gray-100/40 p-4 rounded-xl">
+                      <p>
+                        {typeof dict?.header !== "string" && (
+                          <>{dict?.header.streakMessage}</>
+                        )}
+                      </p>
+                      <Link href={`/${lang}/learn`}>
+                        <button className="bg-yellow-500 hover:bg-yellow-400 dark:bg-yellow-300 dark:hover:bg-yellow-400 hover:border-gray-900/50 dark:hover:border-gray-100/50 focus:border-yellow-500 dark:focus:border-yellow-300 text-sm px-6 py-2 text-gray-900 dark:text-gray-900 rounded-lg font-semibold">
+                          {typeof dict?.header !== "string" && (
+                            <>{dict?.header.streakButton}</>
+                          )}
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </Transition>
               </div>
             </div>
             <div className="flex justify-center items-center mx-4 h-6 w-px rounded-full bg-gray-900/20 dark:bg-gray-100/40" />
@@ -397,9 +465,20 @@ const Header = ({
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/20">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center gap-x-6">
-            <div className="flex flex-1 items-center justify-between gap-x-4">
+            <div className="-m-1.5 p-1.5">
+              <Link href={`/${lang}/home`}>
+                <Image
+                  className="h-12 w-auto"
+                  width={128}
+                  height={128}
+                  src="/assets/logo/trotelcoin-white.png"
+                  alt={""}
+                ></Image>
+              </Link>
+            </div>
+            <div className="flex flex-1 items-center justify-end gap-x-4">
               {/*<Wallet lang={lang} />*/}
 
               {address && isLoggedIn ? (
