5c06d2b23a5f031307a1333c149550ae4f7217fa
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-14 10:31:01 +0100

    add: api route for leaderboard

---
+++ app/api/database/leaderboard/route.ts
@@ -0,0 +1,39 @@
+import { supabase } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const wallet = searchParams.get("wallet");
+
+    // get leaderboard all of learners depending of total rewards
+    const { data: leaderboard, error: leaderboardError } = await supabase
+      .from("learners")
+      .select("*")
+      .order("number_of_quizzes_answered", { ascending: false });
+
+    if (leaderboardError) {
+      console.error(leaderboardError);
+      return new NextResponse(
+        JSON.stringify({ error: "Something went wrong." }),
+        { status: 500 }
+      );
+    }
+
+    // get the position of the learner
+    const position = leaderboard.findIndex(
+      (learner) => learner.wallet === wallet
+    );
+
+    return new NextResponse(
+      JSON.stringify({ leaderboard, position: position + 1 }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return new NextResponse(
+      JSON.stringify({ error: "Something went wrong." }),
+      { status: 500 }
+    );
+  }
+}
