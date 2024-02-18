b94df089fd82b9740e246caabad607d577ab287a
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-18 16:00:15 +0100

    fix: disclaimer

--- app/[lang]/[quizId]/layout.tsx
+++ app/[lang]/[quizId]/layout.tsx
@@ -245,7 +245,8 @@ const CoursePage = ({
               </div>
             </ul>
           </div>
-          <p className="mt-2 text-gray-900 dark:text-gray-100 font-light">
+          <p className="mb-10 text-sm text-gray-900 dark:text-gray-100 font-light">
+            <span className="font-semibold">Disclaimer: </span>
             {typeof dict?.lesson !== "string" && <>{dict?.lesson.disclaimer}</>}
           </p>
           <>{children}</>
