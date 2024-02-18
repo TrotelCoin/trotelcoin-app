5f35d6d81c145a684c9bd2507a67a1cb3f7ad666
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-18 21:32:54 +0100

    fix: api allow cors

---
+++ app/api/courses/coursesCount/route.ts
@@ -0,0 +1,19 @@
+import lessons from "@/data/lessonsData";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const totalCourses = lessons.reduce(
+    (acc, curr) => acc + curr.courses.length,
+    0
+  );
+
+  return NextResponse.json(totalCourses, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "https://www.trotelcoin.com",
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
+}
