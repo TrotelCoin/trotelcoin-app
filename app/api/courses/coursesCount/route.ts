73519aae962a6bc6ea9b5ad578f29ce9d7444258
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-18 22:04:13 +0100

    fix: courses count to get method

--- app/api/courses/coursesCount/route.ts
+++ app/api/courses/coursesCount/route.ts
@@ -1,7 +1,7 @@
 import lessons from "@/data/lessonsData";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   const totalCourses = lessons.reduce(
     (acc, curr) => acc + curr.courses.length,
     0
@@ -10,7 +10,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
   return NextResponse.json(totalCourses, {
     status: 200,
     headers: {
-      "Access-Control-Allow-Origin": "https://www.trotelcoin.com",
+      "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET",
       "Access-Control-Allow-Headers": "Content-Type",
     },
