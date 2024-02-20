6f1a55c236b6e6e17bec2a985eb93479a1f48c27
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-20 19:12:53 +0100

    fix: premium page

---
+++ app/[lang]/premium/components/premiumStatistics.tsx
@@ -0,0 +1,71 @@
+import trotelCoinEarlyABI from "@/abi/trotelCoinEarly";
+import trotelCoinExpertABI from "@/abi/trotelCoinExpert";
+import trotelCoinIntermediateABI from "@/abi/trotelCoinIntermediate";
+import {
+  trotelCoinIntermediateAddress,
+  trotelCoinExpertAddress,
+  trotelCoinEarlyAddress,
+} from "@/data/web3/addresses";
+import { DictType } from "@/types/types";
+import React from "react";
+import { polygon } from "viem/chains";
+import { useContractRead } from "wagmi";
+
+const PremiumStatistics = ({ dict }: { dict: DictType }) => {
+  const { data: intermediate } = useContractRead({
+    chainId: polygon.id,
+    address: trotelCoinIntermediateAddress,
+    abi: trotelCoinIntermediateABI,
+    functionName: "totalSupply",
+    watch: true,
+  });
+  const { data: expert } = useContractRead({
+    chainId: polygon.id,
+    address: trotelCoinExpertAddress,
+    abi: trotelCoinExpertABI,
+    functionName: "totalSupply",
+    watch: true,
+  });
+  const { data: early } = useContractRead({
+    chainId: polygon.id,
+    address: trotelCoinEarlyAddress,
+    abi: trotelCoinEarlyABI,
+    functionName: "totalSupply",
+    watch: true,
+  });
+
+  return (
+    <>
+      <div className="overflow-hidden grid grid-cols-1 md:grid-cols-3 mt-4 text-gray-900 dark:text-gray-100 items-center text-center md:divide-x md:divide-y-0 divide-x-0 divide-y divide-gray-900/20 dark:divide-gray-100/40 rounded-lg bg-gray-50 dark:bg-gray-900 border border-gray-900/20 dark:border-gray-100/40 blackdrop-blur-xl">
+        <div className="items-center flex flex-col py-6">
+          <span className="text-2xl md:text-4xl font-semibold">
+            {parseFloat(early as string)}
+          </span>
+          <span>
+            {typeof dict?.premium !== "string" && <>{dict?.premium.early}</>}
+          </span>
+        </div>
+        <div className="items-center flex flex-col py-6">
+          <span className="text-2xl md:text-4xl font-semibold">
+            {parseFloat(intermediate as string)}
+          </span>
+          <span>
+            {typeof dict?.premium !== "string" && (
+              <>{dict?.premium.intermediate}</>
+            )}
+          </span>
+        </div>
+        <div className="items-center flex flex-col py-6">
+          <span className="text-2xl md:text-4xl font-semibold">
+            {parseFloat(expert as string)}
+          </span>
+          <span>
+            {typeof dict?.premium !== "string" && <>{dict?.premium.expert}</>}
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PremiumStatistics;
--- app/[lang]/premium/page.tsx
+++ app/[lang]/premium/page.tsx
@@ -4,18 +4,9 @@ import React, { useEffect, useState } from "react";
 import Intermediate from "@/app/[lang]/premium/components/intermediate";
 import Expert from "@/app/[lang]/premium/components/expert";
 import Beginner from "@/app/[lang]/premium/components/beginner";
-import { useContractRead } from "wagmi";
-import { polygon } from "wagmi/chains";
-import trotelCoinIntermediateABI from "@/abi/trotelCoinIntermediate";
-import trotelCoinExpertABI from "@/abi/trotelCoinExpert";
-import trotelCoinEarlyABI from "@/abi/trotelCoinEarly";
-import {
-  trotelCoinEarlyAddress,
-  trotelCoinIntermediateAddress,
-  trotelCoinExpertAddress,
-} from "@/data/web3/addresses";
 import { DictType, Lang } from "@/types/types";
 import { getDictionary } from "@/app/[lang]/dictionaries";
+import PremiumStatistics from "./components/premiumStatistics";
 
 const Subscription = ({ params: { lang } }: { params: { lang: Lang } }) => {
   const [dict, setDict] = useState<DictType | null>(null);
@@ -28,28 +19,6 @@ const Subscription = ({ params: { lang } }: { params: { lang: Lang } }) => {
 
     fetchDictionary();
   }, [lang]);
-
-  const { data: intermediate } = useContractRead({
-    chainId: polygon.id,
-    address: trotelCoinIntermediateAddress,
-    abi: trotelCoinIntermediateABI,
-    functionName: "totalSupply",
-    watch: true,
-  });
-  const { data: expert } = useContractRead({
-    chainId: polygon.id,
-    address: trotelCoinExpertAddress,
-    abi: trotelCoinExpertABI,
-    functionName: "totalSupply",
-    watch: true,
-  });
-  const { data: early } = useContractRead({
-    chainId: polygon.id,
-    address: trotelCoinEarlyAddress,
-    abi: trotelCoinEarlyABI,
-    functionName: "totalSupply",
-    watch: true,
-  });
 
   return (
     <>
@@ -65,34 +34,7 @@ const Subscription = ({ params: { lang } }: { params: { lang: Lang } }) => {
         <h1 className="text-xl mt-10 text-gray-900 dark:text-gray-100 font-semibold">
           {typeof dict?.premium !== "string" && <>{dict?.premium.statistics}</>}
         </h1>
-        <div className="overflow-hidden grid grid-cols-1 md:grid-cols-3 mt-4 text-gray-900 dark:text-gray-100 items-center text-center md:divide-x md:divide-y-0 divide-x-0 divide-y divide-gray-900/20 dark:divide-gray-100/40 rounded-lg bg-gray-50 dark:bg-gray-900 border border-gray-900/20 dark:border-gray-100/40 blackdrop-blur-xl">
-          <div className="items-center flex flex-col py-6">
-            <span className="text-2xl md:text-4xl font-semibold">
-              {parseFloat(early as string)}
-            </span>
-            <span>
-              {typeof dict?.premium !== "string" && <>{dict?.premium.early}</>}
-            </span>
-          </div>
-          <div className="items-center flex flex-col py-6">
-            <span className="text-2xl md:text-4xl font-semibold">
-              {parseFloat(intermediate as string)}
-            </span>
-            <span>
-              {typeof dict?.premium !== "string" && (
-                <>{dict?.premium.intermediate}</>
-              )}
-            </span>
-          </div>
-          <div className="items-center flex flex-col py-6">
-            <span className="text-2xl md:text-4xl font-semibold">
-              {parseFloat(expert as string)}
-            </span>
-            <span>
-              {typeof dict?.premium !== "string" && <>{dict?.premium.expert}</>}
-            </span>
-          </div>
-        </div>
+        <PremiumStatistics dict={dict as DictType} />
       </div>
     </>
   );
