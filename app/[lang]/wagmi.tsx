ee33995ee83a334d3baca27c920c34dc44f8c92e
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-21 19:45:34 +0100

    fix: wagmi

--- app/[lang]/wagmi.tsx
+++ app/[lang]/wagmi.tsx
@@ -2,54 +2,19 @@
 
 import React from "react";
 import { polygon, mainnet } from "wagmi/chains";
-import { WagmiConfig, createConfig, configureChains } from "wagmi";
-import { publicProvider } from "wagmi/providers/public";
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
-import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
-import { InjectedConnector } from "wagmi/connectors/injected";
-
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, polygon],
-  [publicProvider()]
-);
+import { http, createConfig, WagmiProvider } from "wagmi";
 
 const config = createConfig({
-  autoConnect: false,
-  connectors: [
-    new MetaMaskConnector({
-      chains: chains,
-      options: {
-        shimDisconnect: true,
-      },
-    }),
-    new CoinbaseWalletConnector({
-      chains: chains,
-      options: {
-        appName: "wagmi",
-      },
-    }),
-    new WalletConnectConnector({
-      chains: chains,
-      options: {
-        projectId: "b0d3d1eb9c28fb7899eba1cff830b2b1",
-      },
-    }),
-    new InjectedConnector({
-      chains: chains,
-      options: {
-        name: "Browser Wallet",
-        shimDisconnect: true,
-      },
-    }),
-  ],
-  publicClient,
-  webSocketPublicClient,
+  chains: [polygon, mainnet],
+  transports: {
+    [polygon.id]: http(),
+    [mainnet.id]: http(),
+  },
 });
 
 export default function Wagmi({ children }: { children: React.ReactNode }) {
   try {
-    return <WagmiConfig config={config}>{children}</WagmiConfig>;
+    return <WagmiProvider config={config}>{children}</WagmiProvider>;
   } catch (error) {
     console.error("Error in Wagmi component:", error);
     return <body>Error occurred. Please try again.</body>;
