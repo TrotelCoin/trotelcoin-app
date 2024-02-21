e141e26782a06d29ca908d5d75c33501a95f3284
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-21 22:34:38 +0100

    add: estimating gas fees

---
+++ app/api/getGasFeeForRewards/route.ts
@@ -0,0 +1,44 @@
+import { publicClient } from "@/lib/viem/client";
+import { NextRequest, NextResponse } from "next/server";
+import trotelCoinV1ABI from "@/abi/trotelCoinV1";
+import { Address, parseEther } from "viem";
+import { trotelCoinAddress } from "@/data/web3/addresses";
+import { centralWalletAddress } from "@/lib/viem/client";
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  const { searchParams } = new URL(req.url);
+  const userAddress = searchParams.get("address");
+  const amount = searchParams.get("amount");
+
+  try {
+    if (!userAddress) {
+      return NextResponse.json(
+        { error: "Please provide a valid user address." },
+        { status: 400 }
+      );
+    }
+
+    if (!amount) {
+      return NextResponse.json(
+        { error: "Please provide a valid amount." },
+        { status: 400 }
+      );
+    }
+
+    const gas = await publicClient.estimateContractGas({
+      address: trotelCoinAddress,
+      abi: trotelCoinV1ABI,
+      functionName: "mint",
+      account: centralWalletAddress.address,
+      args: [userAddress, parseEther(amount)],
+    });
+
+    return NextResponse.json(parseFloat(gas.toString()), { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Something went wrong." },
+      { status: 500 }
+    );
+  }
+}
--- lib/viem/client.ts
+++ lib/viem/client.ts
@@ -1,7 +1,17 @@
-import { createPublicClient, http } from "viem";
+import { createPublicClient, http, createWalletClient, Address } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
 import { polygon } from "viem/chains";
 
 export const publicClient = createPublicClient({
   chain: polygon,
   transport: http(),
 });
+
+export const walletClient = createWalletClient({
+  chain: polygon,
+  transport: http(),
+});
+
+export const centralWalletAddress = privateKeyToAccount(
+  process.env.PRIVATE_KEY_WALLET as Address
+);
