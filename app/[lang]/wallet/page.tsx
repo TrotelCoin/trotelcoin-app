157e061495200dbd568179d6545c41fda0cde252
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-22 02:12:08 +0100

    fix: wording

--- app/[lang]/wallet/page.tsx
+++ app/[lang]/wallet/page.tsx
@@ -23,7 +23,7 @@ const Page = ({ params: { lang } }: { params: { lang: Lang } }) => {
       <div className="mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
         <div>
           <h2 className="text-gray-900 dark:text-gray-100 font-semibold text-xl mt-10">
-            {lang === "en" ? <>Claim</> : <>Récupérer</>}
+            {lang === "en" ? <>Wallet</> : <>Portefeuille</>}
           </h2>
           <Claim lang={lang} />
         </div>
