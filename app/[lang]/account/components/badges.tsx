798c1a17d7c49d03c012d4b9bcff4669470d7bd1
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-26 11:22:27 +0100

    fix: badges

--- app/[lang]/account/components/badges.tsx
+++ app/[lang]/account/components/badges.tsx
@@ -6,6 +6,7 @@ import {
   trotelCoinEarlyAddress,
   trotelCoinExpertAddress,
   trotelCoinIntermediateAddress,
+  trotelCoinStakingV1,
 } from "@/data/web3/addresses";
 import { DictType, Lang } from "@/types/types";
 import { useAddress } from "@thirdweb-dev/react";
@@ -14,6 +15,7 @@ import { polygon } from "viem/chains";
 import { useContractRead, useBalance } from "wagmi";
 import BadgesList from "@/app/[lang]/account/components/badges/badgesList";
 import { useEffect, useState } from "react";
+import trotelCoinStakingV1ABI from "@/abi/trotelCoinStakingV1";
 
 const BadgesSection = ({ dict, lang }: { dict: DictType; lang: Lang }) => {
   const [maxStreak, setMaxStreak] = useState<number | null>(null);
@@ -21,6 +23,10 @@ const BadgesSection = ({ dict, lang }: { dict: DictType; lang: Lang }) => {
   const [trotelCoinBalance, setTrotelCoinBalance] = useState<number | null>(
     null
   );
+  const [stakedTrotelCoins, setStakedTrotelCoins] = useState<number | null>(
+    null
+  );
+  const [duration, setDuration] = useState<number | null>(null);
 
   const address = useAddress();
 
@@ -136,6 +142,30 @@ const BadgesSection = ({ dict, lang }: { dict: DictType; lang: Lang }) => {
     }
   }, [address]);
 
+  const { data: getStakingDataNoTyped } = useContractRead({
+    address: trotelCoinStakingV1,
+    functionName: "stakings",
+    args: [address as Address],
+    chainId: polygon.id,
+    watch: true,
+    enabled: Boolean(address),
+    abi: trotelCoinStakingV1ABI,
+  });
+
+  const getStakingData = getStakingDataNoTyped as any[];
+
+  useEffect(() => {
+    if (getStakingData && address) {
+      const balance = parseFloat(getStakingData[0].toString());
+      const duration = parseFloat(getStakingData[2].toString());
+      setStakedTrotelCoins(balance);
+      setDuration(duration);
+    } else {
+      setStakedTrotelCoins(null);
+      setDuration(null);
+    }
+  }, [getStakingData, address]);
+
   const badges = [
     {
       id: 1,
@@ -244,6 +274,63 @@ const BadgesSection = ({ dict, lang }: { dict: DictType; lang: Lang }) => {
       name: lang === "en" ? "1M TrotelCoins" : "1M TrotelCoins",
       image: "🏦",
       condition: trotelCoinBalance && trotelCoinBalance >= 1000000,
+    },
+    {
+      id: 19,
+      name:
+        lang === "en" ? "100 locked TrotelCoins" : "100 TrotelCoins bloqués",
+      image: "🦀",
+      condition: stakedTrotelCoins && stakedTrotelCoins >= 100,
+    },
+    {
+      id: 20,
+      name: lang === "en" ? "1k locked TrotelCoins" : "1k TrotelCoins bloqués",
+      image: "🐠",
+      condition: stakedTrotelCoins && stakedTrotelCoins >= 1000,
+    },
+    {
+      id: 21,
+      name:
+        lang === "en" ? "10k locked TrotelCoins" : "10k TrotelCoins bloqués",
+      image: "🐬",
+      condition: stakedTrotelCoins && stakedTrotelCoins >= 10000,
+    },
+    {
+      id: 22,
+      name:
+        lang === "en" ? "100k locked TrotelCoins" : "100k TrotelCoins bloqués",
+      image: "🦈",
+      condition: stakedTrotelCoins && stakedTrotelCoins >= 100000,
+    },
+    {
+      id: 23,
+      name: lang === "en" ? "1M locked TrotelCoins" : "1M TrotelCoins bloqués",
+      image: "🐳",
+      condition: stakedTrotelCoins && stakedTrotelCoins >= 1000000,
+    },
+    {
+      id: 24,
+      name: lang === "en" ? "30 days locked" : "30 jours bloqués",
+      image: "😪",
+      condition: duration && duration >= 2592000,
+    },
+    {
+      id: 25,
+      name: lang === "en" ? "3 months locked" : "3 mois bloqués",
+      image: "😴",
+      condition: duration && duration >= 7862400,
+    },
+    {
+      id: 26,
+      name: lang === "en" ? "6 months locked" : "6 mois bloqués",
+      image: "⌛️",
+      condition: duration && duration >= 15724800,
+    },
+    {
+      id: 27,
+      name: lang === "en" ? "1 year locked" : "1 an bloqué",
+      image: "⏳",
+      condition: duration && duration >= 31536000,
     },
   ];
 
