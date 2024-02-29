d95472080ff81d5ca1de4ea39254fe87f5632b53
Alexandre Trotel <trotelalexandre@proton.me>
2024-02-29 08:33:53 +0100

    fix: loading

--- app/[lang]/components/loading.tsx
+++ app/[lang]/components/loading.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { Lang } from "@/types/types";
 import "animate.css";
 import React from "react";
 
-const Loading = () => {
+const Loading = ({ params: { lang } }: { params: { lang: Lang } }) => {
   return (
     <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 flex justify-center items-center h-screen animate__animated animate__flash animate__slower animate__infinite">
-      TrotelCoin's courses are loading...
+      {lang === "en"
+        ? "TrotelCoin's courses are loading..."
+        : "Les cours de TrotelCoin sont en train de charger..."}
     </div>
   );
 };
