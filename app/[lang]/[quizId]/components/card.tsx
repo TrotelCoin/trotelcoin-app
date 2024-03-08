f5f184a876e8b1b7649cc6ca2474633a310de69a
Alexandre Trotel <trotelalexandre@proton.me>
2024-03-08 18:38:36 +0100

    fix: font size

--- app/[lang]/[quizId]/components/card.tsx
+++ app/[lang]/[quizId]/components/card.tsx
@@ -4,8 +4,8 @@ const Card = ({ title, text }: { title: string; text: string }) => {
   return (
     <>
       <div className="break-words whitespace-normal">
-        <h1 className="font-semibold mb-2 md:text-2xl text-gray-900 dark:text-gray-100">{title}</h1>
-        <p className="font-light text-sm md:text-base text-gray-900 dark:text-gray-100">{text}</p>
+        <h1 className="font-semibold mb-2 text-2xl text-gray-900 dark:text-gray-100">{title}</h1>
+        <p className="font-light text-gray-900 dark:text-gray-100">{text}</p>
       </div>
     </>
   );
