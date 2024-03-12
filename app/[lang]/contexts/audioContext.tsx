249eb8ef6f1b35829a0967afc8d175004d5f50f2
Alexandre Trotel <trotelalexandre@proton.me>
2024-03-12 08:10:23 +0100

    add: blue button sound

--- app/[lang]/contexts/audioContext.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const CourseFinishedContext = React.createContext({
-  audioEnabled: true as boolean,
-  setAudioEnabled: {} as React.Dispatch<React.SetStateAction<boolean>>,
-});
-
-export default CourseFinishedContext;
--- app/[lang]/contexts/themeContext.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const ThemeContext = React.createContext({
-  theme: "system" as string,
-  toggleTheme: {} as () => void,
-});
-
-export default ThemeContext;
