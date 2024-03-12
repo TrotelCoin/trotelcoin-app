66b28deeeff849e785510e9564cb99617d334520
Alexandre Trotel <trotelalexandre@proton.me>
2024-03-12 19:48:40 +0100

    add: dot

--- app/[lang]/components/header.tsx
+++ app/[lang]/components/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Transition } from "@headlessui/react";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -17,11 +17,15 @@ import LifeMobile from "@/app/[lang]/components/header/lifeMobile";
 import BlueButton from "@/app/[lang]/components/blueButton";
 import AudioSelector from "@/app/[lang]/components/selectors/audioSelector";
 import BlueSimpleButton from "@/app/[lang]/components/blueSimpleButton";
+import { useAddress } from "@thirdweb-dev/react";
+import StreakContext from "@/app/[lang]/contexts/streakContext";
 
 const Header = ({ lang }: { lang: Lang }) => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const pathname = usePathname();
+  const address = useAddress();
+  const { disabled } = useContext(StreakContext);
 
   const navigation = [
     {
@@ -101,7 +105,7 @@ const Header = ({ lang }: { lang: Lang }) => {
                     : defaultClasses
                 }
               >
-                {item.name as string}
+                {item.name}
               </Link>
             );
           })}
@@ -111,11 +115,16 @@ const Header = ({ lang }: { lang: Lang }) => {
         <div className="hidden lg:flex justify-end flex-1 items-center">
           <div className="items-center flex gap-2">
             <ThemeSwitcher />
-            <BlueButton
-              lang={lang}
-              onClick={() => setMobileMenuOpen(true)}
-              text={lang === "en" ? "Menu" : "Menu"}
-            />
+            <div className="relative">
+              <BlueButton
+                lang={lang}
+                onClick={() => setMobileMenuOpen(true)}
+                text={lang === "en" ? "Menu" : "Menu"}
+              />
+              {(!address || !disabled) && (
+                <div className="w-4 h-4 bg-blue-300 rounded-full absolute -top-1 -right-1 animate__flash animate__animated animate__slower animate__infinite" />
+              )}
+            </div>
           </div>
         </div>
 
@@ -124,11 +133,16 @@ const Header = ({ lang }: { lang: Lang }) => {
           <div className="flex items-center">
             <div className="flex items-center gap-2">
               <ThemeSwitcher />
-              <BlueButton
-                lang={lang}
-                onClick={() => setMobileMenuOpen(true)}
-                text={lang === "en" ? "Menu" : "Menu"}
-              />
+              <div className="relative">
+                <BlueButton
+                  lang={lang}
+                  onClick={() => setMobileMenuOpen(true)}
+                  text={lang === "en" ? "Menu" : "Menu"}
+                />
+                {(!address || !disabled) && (
+                  <div className="w-4 h-4 bg-blue-300 rounded-full absolute -top-1 -right-1 animate__flash animate__animated animate__slower animate__infinite" />
+                )}
+              </div>
             </div>
           </div>
         </div>
