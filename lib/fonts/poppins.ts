18881d7fa79cc1dec050cfda99387add7ed88c94
Alexandre Trotel <trotelalexandre@proton.me>
2024-03-15 08:19:59 +0100

    fix: wording

--- lib/fonts/poppins.ts
+++
@@ -1,6 +0,0 @@
-import { Poppins } from "next/font/google";
-
-export const poppins = Poppins({
-  subsets: ["devanagari"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
