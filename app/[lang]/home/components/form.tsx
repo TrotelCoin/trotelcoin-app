6c3d5f9be0544dc610f9b160ef8a57e6a9cf4763
Alexandre Trotel <trotelalexandre@proton.me>
2024-03-20 17:56:27 +0100

    fix: search

--- app/[lang]/home/components/form.tsx
+++ app/[lang]/home/components/form.tsx
@@ -28,14 +28,17 @@ const Form = ({
           <input
             type="search"
             id="default-search"
-            className="block w-full p-4 pl-14 focus:shadow-xl text-sm text-gray-900 border border-gray-900/10 rounded-full bg-gray-100 dark:bg-gray-800 dark:border-gray-100/10 dark:placeholder-gray-400 dark:text-white focus:outline-none focus:ring-transparent focus:border-gray-900/10 dark:focus:border-gray-100/10 transition-all duration-200 ease-in-out"
+            className="appearance-none block w-full p-4 pl-14 focus:shadow-xl text-sm text-gray-900 border border-gray-900/10 rounded-full bg-gray-100 dark:bg-gray-800 dark:border-gray-100/10 dark:placeholder-gray-400 dark:text-white focus:outline-none focus:ring-transparent focus:border-gray-900/10 dark:focus:border-gray-100/10 transition-all duration-200 ease-in-out"
             placeholder={
               lang === "en"
                 ? "What do you want to learn?"
                 : "Que voulez-vous apprendre ?"
             }
             onChange={(e) => setSearchTerm(e.target.value)}
-            style={{ appearance: "none" }}
+            style={{
+              WebkitAppearance: "none",
+              appearance: "none",
+            }}
           />
         </div>
       </form>
