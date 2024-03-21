0fb793fc68eec35a97ad990411a0012a329de5a9
Alexandre Trotel <trotelalexandre@proton.me>
2024-03-21 11:24:06 +0100

    fix: mobile animation

--- app/[lang]/components/header.tsx
+++ app/[lang]/components/header.tsx
@@ -145,7 +145,7 @@ const Header = ({ lang }: { lang: Lang }) => {
           <div className="fixed inset-0">
             <div className="fixed inset-0 overflow-hidden">
               <div className="absolute inset-0 overflow-hidden">
-                <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full">
                   <Transition.Child
                     as={Fragment}
                     enter="transform transition ease-in-out duration-500"
