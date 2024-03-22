8a47f885e949f6cb648935bfd2fff1bc66df53c4
Alexandre Trotel <trotelalexandre@proton.me>
2024-03-22 20:09:53 +0100

    fix: colors

--- app/[lang]/shop/inventory/page.tsx
+++ app/[lang]/shop/inventory/page.tsx
@@ -6,7 +6,7 @@ const Inventory = ({ params: { lang } }: { params: { lang: Lang } }) => {
   return (
     <>
       <div className="mx-auto max-w-md flex flex-col gap-4">
-        <span className="text-2xl font-semibold">
+        <span className="text-2xl font-semibold text-gray-900 dark:text-gray-100">
           {lang === "en" ? "Inventory" : "Inventaire"}
         </span>
 
--- app/[lang]/shop/items/page.tsx
+++ app/[lang]/shop/items/page.tsx
@@ -6,7 +6,9 @@ const Shop = ({ params: { lang } }: { params: { lang: Lang } }) => {
   return (
     <>
       <div className="mx-auto max-w-md flex flex-col gap-4">
-        <span className="text-2xl font-semibold">{lang === "en" ? "Shop" : "Boutique"}</span>
+        <span className="text-2xl font-semibold text-gray-900 dark:text-gray-100">
+          {lang === "en" ? "Shop" : "Boutique"}
+        </span>
         <ComingSoon lang={lang} />
       </div>
     </>
