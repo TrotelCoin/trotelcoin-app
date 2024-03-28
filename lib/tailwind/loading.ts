dc9e7fe73bc3441ced662913bee0fd792a46ac23
Alexandre Trotel <trotelalexandre@proton.me>
2024-03-28 07:24:23 +0100

    add: notifications close button

--- lib/tailwind/loading.ts
+++
@@ -1,4 +0,0 @@
-import "animate.css";
-
-export const loadingFlashClass: string =
-  "animate__flash animate__infinite animate__slower animate__animated";
--- types/user/premium.ts
+++
@@ -1,4 +0,0 @@
-export interface Tier {
-  en: "Beginner" | "Intermediate" | "Expert";
-  fr: "Débutant" | "Intermédiaire" | "Expert";
-}
