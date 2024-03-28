ae5d9c4eaf23520f84bd1a642c5bb31b1e383749
Alexandre Trotel <trotelalexandre@proton.me>
2024-03-28 11:42:53 +0100

    fix: minor bug

--- app/[lang]/wallet/send/page.tsx
+++ app/[lang]/wallet/send/page.tsx
@@ -150,7 +150,7 @@ const Send = ({ params: { lang } }: { params: { lang: Lang } }) => {
               className={`[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none bg-transparent text-4xl font-semibold text-gray-900 dark:text-gray-100 w-full px-2 py-0 border-transparent rounded-xl focus:outline-none focus:ring-transparent focus:border-transparent ${
                 !address && "cursor-not-allowed"
               }`}
-              value={amount < 0 ? 0 : amount}
+              value={amount === null || amount < 0 ? 0 : amount}
               onChange={(e) => setAmount(parseFloat(e.target.value))}
               onWheel={(e) => (e.target as HTMLInputElement).blur()}
               placeholder={lang === "en" ? "Amount" : "Montant"}
