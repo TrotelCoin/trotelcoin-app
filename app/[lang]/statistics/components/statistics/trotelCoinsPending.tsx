babc9d400b8e315d469927297ea31519004bdcc4
Alexandre Trotel <trotelalexandre@proton.me>
2024-03-30 01:33:28 +0100

    fix: stats

--- app/[lang]/statistics/components/statistics/trotelCoinsPending.tsx
+++ app/[lang]/statistics/components/statistics/trotelCoinsPending.tsx
@@ -26,7 +26,7 @@ const TrotelCoinsPending = ({ lang }: { lang: Lang }) => {
       <div
         className={`bg-gray-50 flex flex-col border backdrop-blur-xl border-gray-900/10 dark:border-gray-100/10 text-center rounded-xl px-2 py-10 dark:bg-gray-800 text-gray-900 dark:text-gray-100`}
       >
-        <Evolution evolution={evolution as number} />
+        <Evolution evolution={evolution as number} percentage={true} />
         <span className="font-semibold text-2xl md:text-4xl">
           {trotelCoinsPending ? (
             <>
