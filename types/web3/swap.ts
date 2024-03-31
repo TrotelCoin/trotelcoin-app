e24bf0749c6b5ef469e711a37812855289f12bb2
Alexandre Trotel <trotelalexandre@proton.me>
2024-03-31 17:58:03 +0200

    fix: courses count

--- types/web3/swap.ts
+++
@@ -1,7 +0,0 @@
-export type Sort = "output" | "gas" | "time";
-
-export type TokenSource = "from" | "to";
-
-export type ChainSource = "from" | "to";
-
-export type Slippage = "1" | "2" | "3";
--- utils/shortenAddress.ts
+++
@@ -1,9 +0,0 @@
-import { Address } from "viem";
-
-const shortenAddress = (address: Address) => {
-  if (!address) return "unknown.eth";
-
-  return `${address.slice(0, 6)}...${address.slice(-4)}`;
-};
-
-export default shortenAddress;
