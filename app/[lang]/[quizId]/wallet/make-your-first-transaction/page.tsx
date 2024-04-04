238d91622e666f1a74dcfd1d2aae3eac9871664e
Alexandre Trotel <trotelalexandre@proton.me>
2024-04-04 16:36:06 +0200

    add: quiz for making their first transaction

--- app/[lang]/[quizId]/wallet/make-your-first-transaction/page.tsx
+++ app/[lang]/[quizId]/wallet/make-your-first-transaction/page.tsx
@@ -1,65 +1,213 @@
 "use client";
 
-import React from "react";
+import type { Lang } from "@/types/lang";
+import Course from "@/app/[lang]/[quizId]/components/course";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import BlueButton from "@/app/[lang]/components/blueButton";
+import { useAccount, useSendTransaction } from "wagmi";
+import { polygon } from "viem/chains";
+import { trotelCoinDAOAddress } from "@/data/web3/addresses";
+import { parseEther } from "viem";
+import Success from "@/app/[lang]/components/modals/success";
+import Fail from "@/app/[lang]/components/modals/fail";
+import Wallet from "@/app/[lang]/components/header/wallet";
 
-const CoursePage = () => {
+const CoursePage = ({ params: { lang } }: { params: { lang: Lang } }) => {
+  const [condition, setCondition] = useState<boolean>(false);
+  const [disabled, setDisabled] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<boolean>(false);
+  const [successMessage, setSuccessMessage] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const { address } = useAccount();
+  const { sendTransactionAsync } = useSendTransaction({
+    mutation: {
+      onSuccess: () => {
+        setCondition(true);
+        setSuccessMessage(true);
+        setIsLoading(false);
+      },
+      onMutate: () => {
+        setIsLoading(true);
+      },
+      onError: () => {
+        setErrorMessage(true);
+        setCondition(false);
+        setIsLoading(false);
+      },
+    },
+  });
+
+  useEffect(() => {
+    if (address) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [address]);
+
+  const cards = {
+    en: [
+      {
+        title: "Introduction",
+        text: "Once upon a time, Alexandre wanted to surprise his friend, Kylian, with some Polygon tokens.",
+      },
+      {
+        title: "Surprise",
+        text: "Excited, Alexandre opened his crypto wallet on his smartphone.",
+      },
+      {
+        title: "Smile",
+        text: "With a smile, he tapped to send tokens. Carefully, he entered Kylian's wallet address and the amount for his small surprise.",
+      },
+      {
+        title: "Memories",
+        text: "Suddenly, memories of their adventures flooded Alexandre's mind as he double-checked. With a determined nod, he confirmed the transaction.",
+      },
+      {
+        title: "Validators",
+        text: "His wallet quickly created a digital signature, entering Polygon's validator network.",
+      },
+      {
+        title: "In the meantime...",
+        text: "Meanwhile, Kylian remained busy, unaware of Alexandre's digital surprise. Validators diligently worked to confirm the transaction in the background until confirmation!",
+      },
+      {
+        title: "Ending",
+        text: "Alexandre imagined Kylian's surprise, symbolizing their friendship digitally. With a smile, he imagined Kylian finding the unexpected gift in his wallet.",
+      },
+      {
+        title: "Challenge",
+        text: "Now, it's your time to make the transaction! You will send us 0.01 MATIC symbolizing your first transaction.",
+      },
+      {
+        title: "Making the transaction",
+        text: (
+          <>
+            <span className="text-xl font-semibold text-gray-900 dark:text-gray-100">
+              Make your first transaction
+            </span>
+            <div className="mt-4">
+              {address ? (
+                <BlueButton
+                  disabled={disabled}
+                  isLoading={isLoading}
+                  lang={lang}
+                  text="Send transaction"
+                  onClick={async () => {
+                    await sendTransactionAsync({
+                      chainId: polygon.id,
+                      to: trotelCoinDAOAddress,
+                      value: parseEther("0.01"),
+                    });
+                  }}
+                />
+              ) : (
+                <Wallet lang={lang} isCentered={true} />
+              )}
+            </div>
+
+            <Success
+              title="Success"
+              message="The transaction has been done. Congrats!"
+              lang={lang}
+              onClose={() => setSuccessMessage(false)}
+              show={successMessage}
+            />
+            <Fail
+              title="Error"
+              message="An error occured"
+              lang={lang}
+              onClose={() => setErrorMessage(false)}
+              show={errorMessage}
+            />
+          </>
+        ),
+      },
+    ],
+    fr: [
+      {
+        title: "Introduction",
+        text: "Un jour, Alexandre voulait surprendre son ami Kylian en lui offrant des jetons Polygon.",
+      },
+      {
+        title: "La surprise",
+        text: "Tout excité, Alexandre a ouvert son portefeuille crypto sur son smartphone.",
+      },
+      {
+        title: "Le sourire",
+        text: "Avec un sourire, il tapota pour envoyer les jetons. Avec précaution, il entra l'adresse du portefeuille de Kylian et le montant pour sa petite surprise.",
+      },
+      {
+        title: "Les souvenirs",
+        text: "D'un coup, les souvenirs de leurs aventures envahissent l'esprit d'Alexandre, qui procède à une double vérification. D'un signe de tête déterminé, il confirme la transaction.",
+      },
+      {
+        title: "Les validateurs",
+        text: "Son portefeuille a rapidement créé une signature numérique, entrant dans le réseau de validateurs de Polygon.",
+      },
+      {
+        title: "En attendant...",
+        text: "Pendant ce temps, Kylian reste occupé, ignorant la surprise numérique d'Alexandre. Les validateurs ont travaillé avec diligence pour confirmer la transaction en arrière-plan jusqu'à la confirmation !",
+      },
+      {
+        title: "La fin",
+        text: "Alexandre imagine la surprise de Kylian, symbolisant numériquement leur amitié. Avec un sourire, il imagine Kylian découvrant le cadeau inattendu dans son portefeuille.",
+      },
+      {
+        title: "Le défi",
+        text: "Maintenant, c'est à vous d'effectuer la transaction ! Vous nous enverrez 0,01 MATIC symbolisant votre première transaction.",
+      },
+      {
+        title: "Faire la transaction",
+        text: (
+          <>
+            <span className="text-xl font-semibold text-gray-900 dark:text-gray-100">
+              Faîtes votre première transaction
+            </span>
+            <div className="mt-4">
+              {address ? (
+                <BlueButton
+                  disabled={disabled}
+                  isLoading={isLoading}
+                  lang={lang}
+                  text="Send transaction"
+                  onClick={async () => {
+                    await sendTransactionAsync({
+                      chainId: polygon.id,
+                      to: trotelCoinDAOAddress,
+                      value: parseEther("0.01"),
+                    });
+                  }}
+                />
+              ) : (
+                <Wallet lang={lang} isCentered={true} />
+              )}
+            </div>
+            <Success
+              title="Succès"
+              message="La transaction a été effectuée. Félicitations!"
+              lang={lang}
+              onClose={() => setSuccessMessage(false)}
+              show={successMessage}
+            />
+            <Fail
+              title="Erreur"
+              message="Une erreur est survenue"
+              lang={lang}
+              onClose={() => setErrorMessage(false)}
+              show={errorMessage}
+            />
+          </>
+        ),
+      },
+    ],
+  };
+
   return (
     <>
-      <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
-        Install your first wallet
-      </h2>
-      <p className="mt-6">
-        Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in.
-        Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris
-        enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus
-        mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
-        Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
-        diam.
-      </p>
-      <figure className="mt-10 border-l border-blue-500 dark:border-blue-300 pl-9">
-        <blockquote className="font-semibold text-gray-900 dark:text-gray-100">
-          <p>
-            “Vel ultricies morbi odio facilisi ultrices accumsan donec lacus
-            purus. Lectus nibh ullamcorper ac dictum justo in euismod. Risus
-            aenean ut elit massa. In amet aliquet eget cras. Sem volutpat enim
-            tristique.”
-          </p>
-        </blockquote>
-        <figcaption className="mt-6 flex gap-x-4">
-          <div className="text-sm leading-6">
-            <strong className="font-semibold text-gray-900 dark:text-gray-100">
-              Alexandre Trotel
-            </strong>{" "}
-            – CEO & Founder
-          </div>
-        </figcaption>
-      </figure>
-      <p className="mt-10">
-        Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim.
-        Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent
-        donec est. Odio penatibus risus viverra tellus varius sit neque erat
-        velit.
-      </p>
-
-      <div className="mt-16 mx-auto">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
-          Configure your wallet
-        </h2>
-        <p className="mt-6">
-          Purus morbi dignissim senectus mattis adipiscing. Amet, massa quam
-          varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales
-          cursus tristique. Tincidunt sed tempus ut viverra ridiculus non
-          molestie. Gravida quis fringilla amet eget dui tempor dignissim.
-          Facilisis auctor venenatis varius nunc, congue erat ac. Cras fermentum
-          convallis quam.
-        </p>
-        <p className="mt-8">
-          Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-          enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-          praesent donec est. Odio penatibus risus viverra tellus varius sit
-          neque erat velit.
-        </p>
-      </div>
+      <Course cards={cards} lang={lang} conditionIsOkay={condition} />
     </>
   );
 };
