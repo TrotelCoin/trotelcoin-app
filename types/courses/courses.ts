6a89ad67e8fca74541bd972fdeda38827166db3e
Alexandre Trotel <trotelalexandre@proton.me>
2024-04-07 21:50:29 +0200

    add: submit course

---
+++ types/courses/courses.ts
@@ -0,0 +1,11 @@
+export type Category = "Web3" | "Finance";
+
+export type Subcategory =
+  | "Crypto"
+  | "Trading"
+  | "DeFi"
+  | "Investment"
+  | "Cryptocurrencies"
+  | "Stocks";
+
+export type Tiers = "Beginner" | "Intermediate" | "Expert";
