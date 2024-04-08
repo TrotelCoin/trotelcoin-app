44ea21b64d02752e16209289e9d9540cd6d17736
Alexandre Trotel <trotelalexandre@proton.me>
2024-04-08 12:16:32 +0200

    change: satisfaction slider

--- app/[lang]/account/components/header/satisfaction.tsx
+++ app/[lang]/account/components/header/satisfaction.tsx
@@ -4,9 +4,10 @@ import axios from "axios";
 import React, { useContext, useState } from "react";
 import UserContext from "@/app/[lang]/contexts/userContext";
 import { useAccount } from "wagmi";
+import * as Slider from "@radix-ui/react-slider";
 
 const Satisfaction = ({ lang }: { lang: Lang }) => {
-  const [selectedNumber, setSelectedNumber] = useState<number | null>(null);
+  const [selectedNumber, setSelectedNumber] = useState<number[]>([5]);
   const [isResultLoading, setIsResultLoading] = useState<boolean>(false);
 
   const { alreadyAnsweredSatisfaction, setAlreadyAnsweredSatisfaction } =
@@ -46,26 +47,47 @@ const Satisfaction = ({ lang }: { lang: Lang }) => {
               : "Recommanderiez-vous TrotelCoin?"}
           </span>
 
-          <div className="grid grid-cols-6 lg:grid-cols-11 gap-2 mx-auto mt-2">
-            {Array.from(Array(11).keys()).map((number, index) => (
-              <div key={index}>
-                <div
-                  onClick={() => setSelectedNumber(number)}
-                  className={`m-1 w-10 h-10 rounded-xl ${
-                    selectedNumber === number
-                      ? "bg-gray-900 hover:bg-gray-700 dark:bg-gray-50 dark:hover:bg-gray-100 text-gray-100 dark:text-gray-900"
-                      : "bg-gray-300 dark:bg-gray-700 hover:bg-gray-400 dark:hover:bg-gray-600 text-gray-900 dark:text-gray-100"
-                  } cursor-pointer text-xl flex items-center justify-center`}
-                >
-                  {number}
-                </div>
+          <div className="w-full mx-auto mt-2 relative">
+            <div className="flex flex-col w-full pl-2">
+              <div className="flex w-full justify-between items-center">
+                {Array.from(Array(11).keys()).map((index) => (
+                  <span
+                    className={`text-sm font-semibold ${
+                      selectedNumber[0] >= index
+                        ? "rainbow-text"
+                        : "text-gray-900 dark:text-gray-100"
+                    }`}
+                    key={index}
+                  >
+                    {index}
+                  </span>
+                ))}
               </div>
-            ))}
+            </div>
+
+            <form>
+              <Slider.Root
+                className="relative flex items-center select-none touch-none h-5"
+                defaultValue={[5]}
+                min={0}
+                max={10}
+                step={1}
+                onValueChange={(e) => setSelectedNumber(e)}
+              >
+                <Slider.Track className="bg-gray-400 relative grow rounded-full h-2">
+                  <Slider.Range className="absolute from-blue-400 to-blue-500 bg-gradient-to-r rounded-full h-full" />
+                </Slider.Track>
+                <Slider.Thumb
+                  className="block w-5 h-5 bg-gray-900 dark:bg-white rounded-full focus:outline-none"
+                  aria-label="Satisfaction"
+                />
+              </Slider.Root>
+            </form>
           </div>
           <div className="mx-auto mt-2">
             <BlueButton
               lang={lang}
-              onClick={() => satisfactionResult(selectedNumber as number)}
+              onClick={() => satisfactionResult(selectedNumber[0])}
               isLoading={isResultLoading}
               text={lang === "en" ? "Submit" : "Envoyer"}
             />
