67324e391e0bb417d101ba6168c94ff450dda6ab
Alexandre Trotel <trotelalexandre@proton.me>
2024-05-10 21:58:02 +0200

    fix: seo optimization

--- app/[lang]/[quizId]/layout.tsx
+++ app/[lang]/[quizId]/layout.tsx
@@ -22,6 +22,69 @@ import { ShareIcon } from "@heroicons/react/20/solid";
 import CourseFinishedContext from "@/contexts/courseFinished";
 import CourseTitle from "@/app/[lang]/components/courses/courseTitle";
 import SeparatorVertical from "@/app/[lang]/components/separator/vertical";
+import type { Metadata, ResolvingMetadata } from "next";
+
+export async function generateMetadata(
+  { params: { lang, quizId } }: { params: { lang: Lang; quizId: number } },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const foundTier = getTierByQuizId(quizId, lessons);
+
+  const currentCourse: Lesson = lessons
+    .flatMap((lesson) => lesson.courses)
+    .find((course) => course.quizId.toString() === quizId.toString()) as Lesson;
+
+  let title = "";
+  let description = "";
+
+  switch (lang) {
+    case "en":
+      title = currentCourse?.title.en;
+      description = currentCourse?.description.en;
+      break;
+    case "fr":
+      title = currentCourse?.title.fr;
+      description = currentCourse?.description.fr;
+      break;
+    default:
+      title = currentCourse?.title.en;
+      description = currentCourse?.description.en;
+  }
+
+  return {
+    title: title,
+    description: description,
+    generator: "Next.js",
+    manifest: "/manifest.json",
+    appleWebApp: true,
+    keywords:
+      "trotelcoin, learn, earn, learn & earn, crypto, bitcoin, ethereum, trotelcoin app, trotelcoin.com, trotelcoin app, trotelcoin app",
+    authors: [{ name: "TrotelCoin" }],
+    robots: "index, follow",
+    openGraph: {
+      title: title,
+      type: "website",
+      locale: "en_US",
+      url: `https://app.trotelcoin.com/${lang}/${quizId}`,
+      siteName: "TrotelCoin App",
+      description: description,
+      images: [
+        {
+          url: "https://app.trotelcoin.com/assets/banner/trotelcoin-banner.png",
+          width: 800,
+          height: 600,
+          alt: "TrotelCoin App",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary",
+      site: "@trotelcoin",
+      creator: "@trotelcoin",
+      title: title,
+    },
+  };
+}
 
 const CoursePage = ({
   params: { lang, quizId },
