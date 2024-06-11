46ce3c876ec909113b8d921f54101b881fa28cfc
Alexandre Trotel <trotelalexandre@proton.me>
2024-06-11 08:38:13 +0200

    add: sponsored courses category

--- app/[lang]/home/page.tsx
+++ app/[lang]/home/page.tsx
@@ -24,6 +24,7 @@ export default function Home({ params: { lang } }: { params: { lang: Lang } }) {
 
   const scrollRefs = useRef<Array<HTMLDivElement | null>>([]);
   const scrollRefNewCourses = useRef<HTMLDivElement | null>(null);
+  const scrollRefSponsoredCourses = useRef<HTMLDivElement | null>(null);
 
   const filterLessons = (lesson: Lessons) => {
     const categoryMatch = filterByCategory(lesson, searchTerm);
@@ -122,9 +123,42 @@ export default function Home({ params: { lang } }: { params: { lang: Lang } }) {
     }
   };
 
+  const scrollLeftSponsoredCourses = () => {
+    const currentRef = scrollRefSponsoredCourses.current;
+    if (currentRef) {
+      if (currentRef.scrollLeft > 0) {
+        currentRef.scrollBy({
+          left: -currentRef.clientWidth,
+          behavior: "smooth",
+        });
+      }
+    }
+  };
+
+  const scrollRightSponsoredCourses = () => {
+    const currentRef = scrollRefSponsoredCourses.current;
+    if (currentRef) {
+      if (
+        currentRef.scrollLeft <
+        currentRef.scrollWidth - currentRef.clientWidth
+      ) {
+        currentRef.scrollBy({
+          left: currentRef.clientWidth,
+          behavior: "smooth",
+        });
+      }
+    }
+  };
+
   const newCourses = lessons.flatMap((lesson) =>
     lesson.courses
       .filter((course) => course.new)
+      .map((course) => ({ ...course, category: lesson.category }))
+  );
+
+  const sponsoredCourses = lessons.flatMap((lesson) =>
+    lesson.courses
+      .filter((course) => course.sponsored)
       .map((course) => ({ ...course, category: lesson.category }))
   );
 
@@ -137,60 +171,125 @@ export default function Home({ params: { lang } }: { params: { lang: Lang } }) {
           searchTerm={searchTerm}
           filteredLessons={filteredLessons}
         />
-        <div className="my-10">
-          <div className="flex justify-between items-center">
-            <div className="flex items-center gap-2">
-              <h2 className="font-semibold text-xl text-gray-900 dark:text-gray-100">
-                {lang === "en" ? "New Courses ⏳" : "Nouveaux Cours ⏳"}
-              </h2>
+
+        {newCourses && newCourses.length > 0 && (
+          <div className="my-10">
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-2">
+                <h2 className="font-semibold text-xl text-gray-900 dark:text-gray-100">
+                  {lang === "en" ? "New Courses" : "Nouveaux Cours"}
+                </h2>
+              </div>
+              <div className="hidden md:flex items-center gap-2">
+                <button
+                  className="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700 border border-gray-900/10 dark:border-gray-100/10 text-xs text-gray-900 dark:text-gray-100 p-1 text-center flex justify-center items-center rounded-full"
+                  onClick={() => scrollLeftNewCourses()}
+                >
+                  <ChevronLeftIcon className="h-4 w-4 text-black dark:text-white" />
+                </button>
+                <button
+                  className="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700 border border-gray-900/10 dark:border-gray-100/10 text-xs text-gray-900 dark:text-gray-100 p-1 text-center flex justify-center items-center rounded-full"
+                  onClick={() => scrollRightNewCourses()}
+                >
+                  <ChevronRightIcon className="h-4 w-4 text-black dark:text-white" />
+                </button>
+              </div>
             </div>
-            <div className="hidden md:flex items-center gap-2">
-              <button
-                className="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700 border border-gray-900/10 dark:border-gray-100/10 text-xs text-gray-900 dark:text-gray-100 p-1 text-center flex justify-center items-center rounded-full"
-                onClick={() => scrollLeftNewCourses()}
-              >
-                <ChevronLeftIcon className="h-4 w-4 text-black dark:text-white" />
-              </button>
-              <button
-                className="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700 border border-gray-900/10 dark:border-gray-100/10 text-xs text-gray-900 dark:text-gray-100 p-1 text-center flex justify-center items-center rounded-full"
-                onClick={() => scrollRightNewCourses()}
-              >
-                <ChevronRightIcon className="h-4 w-4 text-black dark:text-white" />
-              </button>
+            <div
+              ref={scrollRefNewCourses}
+              className="mt-4 overflow-x-auto flex items-center gap-4 scroll-smooth hide-scrollbar"
+            >
+              {newCourses
+                .sort((a: Lesson, b: Lesson) => {
+                  const tierOrder = {
+                    Beginner: 2,
+                    Intermediate: 1,
+                    Expert: 0,
+                  };
+                  return tierOrder[a.tier.en] - tierOrder[b.tier.en];
+                })
+                .filter((course: Lesson) => {
+                  const lowerCaseTitle = course.title[lang].toLowerCase();
+                  return (
+                    lowerCaseTitle.includes(searchTerm) && course.available
+                  );
+                })
+                .slice(0, 10)
+                .map((course: Lesson, index: number) =>
+                  renderCourses(
+                    course,
+                    isIntermediate,
+                    isExpert,
+                    lang,
+                    course.quizId,
+                    status,
+                    index,
+                    course.category as string
+                  )
+                )}
             </div>
           </div>
-          <div
-            ref={scrollRefNewCourses}
-            className="mt-4 overflow-x-auto flex items-center gap-4 scroll-smooth hide-scrollbar"
-          >
-            {newCourses
-              .sort((a: Lesson, b: Lesson) => {
-                const tierOrder = {
-                  Beginner: 2,
-                  Intermediate: 1,
-                  Expert: 0,
-                };
-                return tierOrder[a.tier.en] - tierOrder[b.tier.en];
-              })
-              .filter((course: Lesson) => {
-                const lowerCaseTitle = course.title[lang].toLowerCase();
-                return lowerCaseTitle.includes(searchTerm) && course.available;
-              })
-              .slice(0, 10)
-              .map((course: Lesson, index: number) =>
-                renderCourses(
-                  course,
-                  isIntermediate,
-                  isExpert,
-                  lang,
-                  course.quizId,
-                  status,
-                  index,
-                  course.category as string
-                )
-              )}
+        )}
+
+        {sponsoredCourses && sponsoredCourses.length > 0 && (
+          <div className="my-10">
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-2">
+                <h2 className="font-semibold text-xl text-gray-900 dark:text-gray-100">
+                  {lang === "en" ? "Sponsored" : "Sponsorisé"}
+                </h2>
+              </div>
+              <div className="hidden md:flex items-center gap-2">
+                <button
+                  className="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700 border border-gray-900/10 dark:border-gray-100/10 text-xs text-gray-900 dark:text-gray-100 p-1 text-center flex justify-center items-center rounded-full"
+                  onClick={() => scrollLeftSponsoredCourses()}
+                >
+                  <ChevronLeftIcon className="h-4 w-4 text-black dark:text-white" />
+                </button>
+                <button
+                  className="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700 border border-gray-900/10 dark:border-gray-100/10 text-xs text-gray-900 dark:text-gray-100 p-1 text-center flex justify-center items-center rounded-full"
+                  onClick={() => scrollRightSponsoredCourses()}
+                >
+                  <ChevronRightIcon className="h-4 w-4 text-black dark:text-white" />
+                </button>
+              </div>
+            </div>
+            <div
+              ref={scrollRefSponsoredCourses}
+              className="mt-4 overflow-x-auto flex items-center gap-4 scroll-smooth hide-scrollbar"
+            >
+              {sponsoredCourses
+                .sort((a: Lesson, b: Lesson) => {
+                  const tierOrder = {
+                    Beginner: 2,
+                    Intermediate: 1,
+                    Expert: 0,
+                  };
+                  return tierOrder[a.tier.en] - tierOrder[b.tier.en];
+                })
+                .filter((course: Lesson) => {
+                  const lowerCaseTitle = course.title[lang].toLowerCase();
+                  return (
+                    lowerCaseTitle.includes(searchTerm) && course.available
+                  );
+                })
+                .slice(0, 10)
+                .map((course: Lesson, index: number) =>
+                  renderCourses(
+                    course,
+                    isIntermediate,
+                    isExpert,
+                    lang,
+                    course.quizId,
+                    status,
+                    index,
+                    course.category as string
+                  )
+                )}
+            </div>
           </div>
-        </div>
+        )}
+
         <div className="flex flex-col">
           {filteredLessons
             .filter((lesson) =>
